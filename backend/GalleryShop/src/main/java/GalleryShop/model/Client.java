0406b026406d2125b0ae54587f9a970803487b63
Dieinimy <dieinimy@gmail.com>
2020-05-01 11:08:49 -0300

    Incluindo herença da classe Person

--- backend/GalleryShop/src/main/java/GalleryShop/model/Client.java
+++ backend/GalleryShop/src/main/java/GalleryShop/model/Client.java
@@ -1,23 +1,29 @@
 package GalleryShop.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Client {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+public class Client extends Person{
 	
-	private String name;
-	
-	private String lastName;
-	
-	private String address;
+	private String email;
 
-	
-	
+	public Client(long id, String name, String lastName, String nickname, String cpf, Date birthdate,
+			String phoneNumber, String email) {
+		super(id, name, lastName, nickname, cpf, birthdate, phoneNumber);
+		this.email = email;
+	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
