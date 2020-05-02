17af56b3863b64134df0fb92d6a7609f397c8c12
Dieinimy <dieinimy@gmail.com>
2020-05-02 09:08:31 -0300

    Criando repository de cliente

---
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package GalleryShop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import GalleryShop.model.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+
+}
