a0c8cb968da12f9c7312f7edddafd6bc54320e99
Dieinimy <dieinimy@gmail.com>
2020-05-02 10:02:34 -0300

    Criado ClientDto

---
+++ backend/GalleryShop/src/main/java/GalleryShop/controller/dto/ClientDto.java
@@ -0,0 +1,93 @@
+package GalleryShop.controller.dto;
+
+import java.util.Date;
+
+import org.springframework.data.domain.Page;
+
+import GalleryShop.model.Client;
+
+public class ClientDto {
+	
+	private Long id;
+	private String name;
+	private String lastName;
+	private String nickname;
+	private String cpf;
+	private Date birthdate;
+	private String phoneNumber;
+	private String email;
+	
+	
+	public ClientDto(Client client) {
+		this.id = client.getId();
+		this.name = client.getName();
+		this.lastName = client.getLastName();
+		this.nickname = client.getNickname();
+		this.cpf = client.getCpf();
+		this.birthdate = client.getBirthdate();
+		this.phoneNumber = client.getPhoneNumber();
+		this.email = client.getEmail();
+	}
+	
+		
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	public static Page<ClientDto> converter(Page<Client> clients) {
+
+		return clients.map(ClientDto::new);
+	}
+	
+
+}
+
