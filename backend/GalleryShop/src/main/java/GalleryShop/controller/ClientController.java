5143c7838bc9ea608ac905c6a9d17e5b831760d0
Dieinimy <dieinimy@gmail.com>
2020-05-02 10:30:57 -0300

    Corrigindo importação das bibliotecas e finalizando o getAll

--- backend/GalleryShop/src/main/java/GalleryShop/controller/ClientController.java
+++ backend/GalleryShop/src/main/java/GalleryShop/controller/ClientController.java
@@ -1,28 +1,26 @@
 package GalleryShop.controller;
 
 
-
-import java.awt.print.Pageable;
-import java.net.URI;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import GalleryShop.controller.dto.ClientDto;
 import GalleryShop.controller.form.ClientForm;
 import GalleryShop.model.Client;
 import GalleryShop.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping("/clients")
@@ -32,8 +30,9 @@ public class ClientController {
 	private ClientRepository clientRepository;
 	
 	@GetMapping
-	public Page<ClientDto> getListAll(){
-		Page<Client> clients = (Page<Client>) clientRepository.findAll();
+	public Page<ClientDto> getListAll(@PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10)
+												  Pageable paginacao){
+		Page<Client> clients = clientRepository.findAll(paginacao);
 		return ClientDto.converter(clients);
 	}
 	
