64a1ec7cae5fd5c3781e56be845334e08c6227db
Dieinimy <dieinimy@gmail.com>
2020-05-03 11:41:27 -0300

    Incluido busca por CPF no repository

--- backend/GalleryShop/src/main/java/GalleryShop/repository/ClientRepository.java
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/ClientRepository.java
@@ -1,10 +1,13 @@
 package GalleryShop.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import GalleryShop.model.Client;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long>{
 
-
+    Optional<Client> findByCpf(String cpf);
 }
