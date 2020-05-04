cd39105d028fae4b4c0c33c7a9d221e74f48d2e4
Dieinimy <dieinimy@gmail.com>
2020-05-04 19:42:06 -0300

    Finalizando tela e incluindo botão de cadastrar

--- frontend/galleryshop/lib/screens/FormCreateNewClient.dart
+++ frontend/galleryshop/lib/screens/FormCreateNewClient.dart
@@ -11,6 +11,13 @@ const _tipFieldLastName = 'Digite o Sobrenome';
 const _labelFieldNickname = 'Apelido';
 const _tipFieldNickname = 'Digite o apelido';
 const _labelFieldCpf = 'CPF';
+const _tipFieldCpf = '000.000.000-00';
+const _labelFieldBirhDate = 'Data Nascimento';
+const _tipFieldBirhdate = '00/00/0000';
+const _labelFieldPhoneNumber = 'Telefone';
+const _tipFieldPhoneNumber = '(000) 00000-0000';
+const _labelFieldEmail = 'Email';
+const _tipFieldEmail = 'Digite o e-mail';
 
 class FormCreateNewClient extends StatefulWidget {
   @override
@@ -23,9 +30,14 @@ class _FormCreateNewClientState extends State<FormCreateNewClient> {
       TextEditingController();
   final TextEditingController _controllerFieldNickname =
       TextEditingController();
-  final TextEditingController _controllerFieldCpf = TextEditingController();
   final MaskedTextController _controllerMaskFieldCpf =
-      new MaskedTextController(mask: '000.000.000-78');
+      new MaskedTextController(mask: '000.000.000-00');
+
+  final MaskedTextController _controllerMaskFieldPhoneNumber =
+      new MaskedTextController(mask: '(000) 00000-0000');
+  final MaskedTextController _controllerMaskFieldBirthDate =
+      new MaskedTextController(mask: '00/00/0000');
+  final TextEditingController _controllerFieldEmail = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -56,10 +68,41 @@ class _FormCreateNewClientState extends State<FormCreateNewClient> {
             ),
             Editor(
               controller: _controllerMaskFieldCpf,
+              tip: _tipFieldCpf,
               label: _labelFieldCpf,
               textInputType: TextInputType.number,
             ),
+            Editor(
+              controller: _controllerMaskFieldBirthDate,
+              tip: _tipFieldBirhdate,
+              label: _labelFieldBirhDate,
+              textInputType: TextInputType.number,
+            ),
+            Editor(
+              controller: _controllerMaskFieldPhoneNumber,
+              tip: _tipFieldPhoneNumber,
+              label: _labelFieldPhoneNumber,
+              textInputType: TextInputType.number,
+            ),
+            Editor(
+              controller: _controllerFieldName,
+              tip: _tipFieldEmail,
+              label: _labelFieldEmail,
+              textInputType: TextInputType.emailAddress,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: ButtonTheme(
+                minWidth: 200.0,
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {  },
+                  child: Text('Cadastrar'),
+                ),
+              ),
+            )
           ],
+
         ),
       ),
     );
