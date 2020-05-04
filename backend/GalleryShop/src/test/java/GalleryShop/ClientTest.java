2611b90b6ded11c0a3c391ac1d15ce55176d2185
Dieinimy <dieinimy@gmail.com>
2020-05-04 18:17:04 -0300

    Iniciando front

---
+++ backend/GalleryShop/src/test/java/GalleryShop/ClientTest.java
@@ -0,0 +1,14 @@
+package GalleryShop;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClientTest {
+	
+	@Test
+	public void createClient() {
+		
+	}
+
+}
---
+++ frontend/galleryshop/android/app/src/main/kotlin/com/example/galleryshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.galleryshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
