8b87347049169365fefb4e733d7166f54cfc7dd1
Dieinimy <dieinimy@gmail.com>
2020-05-07 17:52:05 -0300

    Criado repository de TypeEmployee

---
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/TypeEmployeeRepository.java
@@ -0,0 +1,9 @@
+package GalleryShop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import GalleryShop.model.TypeEmployee;
+
+public interface TypeEmployeeRepository extends JpaRepository<TypeEmployee, Long> {
+
+}
