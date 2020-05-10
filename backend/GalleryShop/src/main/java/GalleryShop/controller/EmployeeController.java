34e5cbbbb3208d533ebf3a322e63736a3c38251b
Dieinimy <dieinimy@gmail.com>
2020-05-10 11:36:05 -0300

    Criado metodo post para Funcionario

--- backend/GalleryShop/src/main/java/GalleryShop/controller/EmployeeController.java
+++ backend/GalleryShop/src/main/java/GalleryShop/controller/EmployeeController.java
@@ -1,16 +1,25 @@
 package GalleryShop.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import GalleryShop.controller.dto.EmployeeDto;
+import GalleryShop.controller.form.EmployeeForm;
 import GalleryShop.model.Employee;
 import GalleryShop.repository.EmployeeRepository;
 import GalleryShop.repository.TypeEmployeeRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/employees")
@@ -23,9 +32,24 @@ public class EmployeeController {
     TypeEmployeeRepository typeEmployeeRepository;
 
     @GetMapping
-    public List<EmployeeDto> getAll(){
+    public List<EmployeeDto> getAll() {
         List<Employee> employees = employeeRepository.findAll();
         return EmployeeDto.converter(employees);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<EmployeeDto> createNewEmployee(@RequestBody @Valid EmployeeForm form,
+            UriComponentsBuilder uriBuilder) {
+        Employee employee = form.converter(typeEmployeeRepository);
+        if (employee.getTypeEmployees() == null) {
+            return ResponseEntity.noContent().build();
+        } else {
+            employeeRepository.save(employee);
+            URI uri = uriBuilder.path("/employees/{id}").buildAndExpand(employee.getId()).toUri();
+            return ResponseEntity.created(uri).body(new EmployeeDto(employee));
+        }
+
     }
 
 }
