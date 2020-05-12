428300bb2e1e18b0acd029f9cd24f2dc27393657
Dieinimy <dieinimy@gmail.com>
2020-05-12 19:36:05 -0300

    Incluido formulario de login para testes

--- backend/GalleryShop/src/main/java/GalleryShop/config/security/SecurityConfiguration.java
+++ backend/GalleryShop/src/main/java/GalleryShop/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package GalleryShop.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,14 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     // Configurações de autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
 
     }
 
@@ -22,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/employees").permitAll()
-                .antMatchers(HttpMethod.GET, "/employees/*").permitAll().anyRequest().authenticated();
+                .antMatchers(HttpMethod.GET, "/employees/*").permitAll().anyRequest().authenticated().and().formLogin();
 
     }
 
@@ -31,5 +37,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
 
     }
+    // Pra gerar senha, só pra implementar no form de usuário
+    // public static void main(String[] args) {
+    // System.out.println(new BCryptPasswordEncoder().encode("aquario"));
+    // }
 
 }
