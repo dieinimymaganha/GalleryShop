6179b81823249271f799c0cb668533e6b6945cf4
Dieinimy <dieinimy@gmail.com>
2020-05-12 20:37:02 -0300

    Dando continuidade na criação do metodo post...Precisa ver melhor essa questão

--- backend/GalleryShop/src/main/java/GalleryShop/controller/AuthenticationController.java
+++ backend/GalleryShop/src/main/java/GalleryShop/controller/AuthenticationController.java
@@ -1,25 +1,45 @@
 package GalleryShop.controller;
 
+import javax.security.sasl.AuthenticationException;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import GalleryShop.config.security.TokenService;
 import GalleryShop.controller.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> authenticate(@RequestBody @Valid LoginForm form) {
-        System.out.println(form.getEmail());
-        System.out.println(form.getPassword());
+        UsernamePasswordAuthenticationToken dataLogin = form.converter();
+        // try {
+        Authentication authentication = authManager.authenticate(dataLogin);
+        String token = tokenService.generateToken(authentication);
+        System.out.println(token);
 
         return ResponseEntity.ok().build();
+
+        // } catch (AuthenticationException e) {
+        // return ResponseEntity.badRequest().build();
+
+        // }
 
     }
 
