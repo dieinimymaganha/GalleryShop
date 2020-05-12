3aa67e57a4f473df4d4d6cab8228033476c823eb
Dieinimy <dieinimy@gmail.com>
2020-05-12 17:44:20 -0300

    Incluido dependencia  e feita primeira configuração

---
+++ backend/GalleryShop/src/main/java/GalleryShop/config/security/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package GalleryShop.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    
+
+    
+}
