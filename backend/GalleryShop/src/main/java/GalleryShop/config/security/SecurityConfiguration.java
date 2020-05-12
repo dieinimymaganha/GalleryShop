7a328c1848485bc622b2268a989dedb6ca1e3c71
Dieinimy <dieinimy@gmail.com>
2020-05-12 19:08:13 -0300

    Incluida configurações no Security

--- backend/GalleryShop/src/main/java/GalleryShop/config/security/SecurityConfiguration.java
+++ backend/GalleryShop/src/main/java/GalleryShop/config/security/SecurityConfiguration.java
@@ -1,13 +1,35 @@
 package GalleryShop.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-    
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    
+    // Configurações de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    // Configurações de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/employees").permitAll()
+                .antMatchers(HttpMethod.GET, "/employees/*").permitAll().anyRequest().authenticated();
+
+    }
+
+    // Configurações recursos estático
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
 }
