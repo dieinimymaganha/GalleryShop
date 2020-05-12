9028e6e69159d08ec0a6e1bc28e4c0f475b31ec3
Dieinimy <dieinimy@gmail.com>
2020-05-12 19:59:30 -0300

    Removendo dados não utilizados

--- backend/GalleryShop/src/main/java/GalleryShop/model/Profile.java
+++ backend/GalleryShop/src/main/java/GalleryShop/model/Profile.java
@@ -1,14 +1,9 @@
 package GalleryShop.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 import org.springframework.security.core.GrantedAuthority;
 
@@ -22,8 +17,6 @@ public class Profile implements GrantedAuthority {
     private Long id;
 
     private String name;
-    // @ManyToMany(mappedBy = "profiles", cascade = CascadeType.ALL)
-    // private List<UserLogin> usersLogin = new ArrayList<>();
 
     @Override
     public String getAuthority() {
