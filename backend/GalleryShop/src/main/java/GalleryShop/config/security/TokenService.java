2c8b514fcc7415250c0f74b21487d29daec66a2c
Dieinimy <dieinimy@gmail.com>
2020-05-13 20:00:58 -0300

    Criado função para validar se o token é valido

--- backend/GalleryShop/src/main/java/GalleryShop/config/security/TokenService.java
+++ backend/GalleryShop/src/main/java/GalleryShop/config/security/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import GalleryShop.model.UserLogin;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -26,6 +27,21 @@ public class TokenService {
 
         return Jwts.builder().setIssuer("API GalleryShop").setSubject(logged.getId().toString()).setIssuedAt(today)
                 .setExpiration(dateExpiration).signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public Long getIdUser(String token) {
+        Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
     }
 
 }
