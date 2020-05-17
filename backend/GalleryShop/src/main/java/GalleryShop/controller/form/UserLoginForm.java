04e4f877f16ce7d5fbfd6aab33cbecbef5545987
Dieinimy <dieinimy@gmail.com>
2020-05-17 16:00:08 -0300

    Incluido perfil de usuário

--- backend/GalleryShop/src/main/java/GalleryShop/controller/form/UserLoginForm.java
+++ backend/GalleryShop/src/main/java/GalleryShop/controller/form/UserLoginForm.java
@@ -1,17 +1,22 @@
 package GalleryShop.controller.form;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import GalleryShop.model.Client;
+import GalleryShop.model.Profile;
 import GalleryShop.model.UserLogin;
 import GalleryShop.repository.ClientRepository;
+import GalleryShop.repository.ProfileRepository;
 
 public class UserLoginForm {
 
     private String phoneNumber;
     private String password;
+    private List<Profile> listProfiles = new ArrayList<>();
 
     public String getPhoneNumber() {
         return phoneNumber;
@@ -29,11 +34,28 @@ public class UserLoginForm {
         this.password = password;
     }
 
-    public UserLogin coverter(ClientRepository clientRepository) {
+    public List<Profile> getListProfiles() {
+        return listProfiles;
+    }
+
+    public void setListProfiles(List<Profile> listProfiles) {
+        this.listProfiles = listProfiles;
+    }
+
+    public UserLogin coverter(ClientRepository clientRepository, ProfileRepository profileRepository) {
+
+        List<Profile> newListProfile = new ArrayList<>();
+
+        for (Profile profile : listProfiles) {
+            Profile newProfile = profileRepository.findByRole(profile.getRole().toString());
+            newListProfile.add(newProfile);
+        }
+
         Optional<Client> client = clientRepository.findByPhoneNumber(phoneNumber);
+
         if (client.isPresent()) {
             String passwordCrypt = new BCryptPasswordEncoder().encode(password);
-            return new UserLogin(phoneNumber, passwordCrypt, client.get());
+            return new UserLogin(phoneNumber, passwordCrypt, client.get(), newListProfile);
         }
 
         return null;
