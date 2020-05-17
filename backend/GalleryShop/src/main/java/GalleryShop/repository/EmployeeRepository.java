fb17fcf6dba4ad24461d76dc4b3f456c4b924ac5
Dieinimy <dieinimy@gmail.com>
2020-05-17 16:09:51 -0300

    Criado busca por phoneNumber

--- backend/GalleryShop/src/main/java/GalleryShop/repository/EmployeeRepository.java
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package GalleryShop.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import GalleryShop.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByPhoneNumber(String phoneNumber);
 
 }
