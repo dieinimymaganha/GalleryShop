55c8f44bab98e0ad6161c8052f225fd61293e421
Dieinimy <dieinimy@gmail.com>
2020-05-18 18:23:45 -0300

    Incluido serviço como unico

--- backend/GalleryShop/src/main/java/GalleryShop/model/Service.java
+++ backend/GalleryShop/src/main/java/GalleryShop/model/Service.java
@@ -1,5 +1,6 @@
 package GalleryShop.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
     private String description;
     private Double value;
     private Boolean fixedPrice;
