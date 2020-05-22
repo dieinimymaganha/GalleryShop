7a42bdb5032aae4d7434fedaee28a9cdc120038e
Dieinimy <dieinimy@gmail.com>
2020-05-22 12:32:49 -0300

    Alterado para incluir  se o campo é obrigatório

--- frontend/galleryshopcustomers/lib/components/custom_form.dart
+++ frontend/galleryshopcustomers/lib/components/custom_form.dart
@@ -7,37 +7,49 @@ class CustomForm extends StatelessWidget {
   final Widget icon;
   final TextInputType textInputType;
   final bool obscure;
+  final bool mandatory;
 
-  CustomForm(
-      {this.controller,
-      this.label,
-      this.tip,
-      this.icon,
-      this.textInputType,
-      this.obscure});
+  CustomForm({
+    this.controller,
+    this.label,
+    this.tip,
+    this.icon,
+    this.textInputType,
+    this.obscure,
+    this.mandatory,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      style: TextStyle(color: Color(0xFFF234253), fontWeight: FontWeight.bold),
-      controller: controller,
-      obscureText: obscure,
-      decoration: InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+        style:
+            TextStyle(color: Color(0xFFF234253), fontWeight: FontWeight.bold),
+        controller: controller,
+        obscureText: obscure,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+          labelStyle: TextStyle(
+            color: Colors.black38,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+          labelText: label,
+          hintText: tip,
+          suffixIcon: icon,
         ),
-        enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)),
-        labelStyle: TextStyle(
-          color: Colors.black38,
-          fontWeight: FontWeight.w400,
-          fontSize: 20,
-        ),
-        labelText: label,
-        hintText: tip,
-        suffixIcon: icon,
-      ),
-      keyboardType: textInputType,
-    );
+        keyboardType: textInputType,
+        validator: (value) {
+          if (mandatory == true) {
+            if (value.isEmpty) {
+              return 'Campo obrigatório';
+            }
+            return null;
+          }
+          return null;
+        });
   }
 }
