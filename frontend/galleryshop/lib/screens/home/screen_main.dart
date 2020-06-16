08c9f4349613dc9a7073a2bfda1932cb96846644
Dieinimy <dieinimy@gmail.com>
2020-06-16 18:27:47 -0300

    Removido todos os conteúdos da pagina inicial

--- frontend/galleryshop/lib/screens/home/screen_main.dart
+++ frontend/galleryshop/lib/screens/home/screen_main.dart
@@ -21,66 +21,10 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: colorAppbar,
       ),
       backgroundColor: Colors.white10,
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage("images/background.jpg"),
-              fit: BoxFit.cover,
-            )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 24),
-                    child: Column(
-                      children: <Widget>[
-                        buildOptions(),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+      body: Container(
+        color: Colors.white,
       ),
       drawer: CustomDrawer(),
-    );
-  }
-
-  Widget buildOptions() {
-    return Container(
-      height: 120,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              OptionCard('Agenda', Icons.calendar_today, () {
-                Navigator.of(context).pushNamed('/tracks');
-              }),
-              OptionCard('Conta', Icons.account_balance_wallet, () {
-                Navigator.of(context).pushNamed('/members');
-              }),
-              OptionCard('Perfil', Icons.account_box, () {}),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
