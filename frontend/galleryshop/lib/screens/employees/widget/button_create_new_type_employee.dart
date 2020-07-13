1b75ef1ac69797f8f4897c31d49a6a9d603240ef
Dieinimy <dieinimy@gmail.com>
2020-07-13 19:20:44 -0300

    Alterado para exibir o Dialog para cadastrar novo typeEmployee

--- frontend/galleryshop/lib/screens/employees/widget/button_create_new_type_employee.dart
+++ frontend/galleryshop/lib/screens/employees/widget/button_create_new_type_employee.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:galleryshop/data/values.dart';
 import 'package:galleryshop/screens/client/create_new_user_client_screen.dart';
 
+import '../edit_type_employee.dart';
+
 class ButtonCreateNewTypeEmployee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       child: Icon(
         Icons.add,
-
       ),
       onPressed: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => CreateNewUserClientScreen()));
+        showDialog(
+            context: context, builder: (context) => EditTypeEmployeeDialog());
       },
       backgroundColor: colorAppbar,
     );
