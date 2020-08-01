3ab3c033fd0b165d8f5d10f4490a5b5ef6230abe
Dieinimy <dieinimy@gmail.com>
2020-08-01 11:52:50 -0300

    Criado formulário só para moedas

---
+++ frontend/galleryshop/lib/widgets/custom_form_coin.dart
@@ -0,0 +1,67 @@
+import 'package:brasil_fields/formatter/real_input_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomFormCoin extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String tip;
+  final Widget icon;
+  final TextInputType textInputType;
+  final bool mandatory;
+  final Function(String) onChanged;
+  final bool enabled;
+  final Widget suffix;
+  final int maxlength_field;
+
+  CustomFormCoin({
+    this.controller,
+    this.label,
+    this.tip,
+    this.icon,
+    this.textInputType,
+    this.mandatory,
+    this.onChanged,
+    this.enabled,
+    this.suffix,
+    this.maxlength_field,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(maxlength_field),
+          WhitelistingTextInputFormatter.digitsOnly,
+          RealInputFormatter(centavos: true),
+        ],
+        style:
+            TextStyle(color: Color(0xFFF234253), fontWeight: FontWeight.bold),
+        controller: controller,
+        onChanged: onChanged,
+        enabled: enabled,
+        decoration: InputDecoration(
+          prefixIcon: icon,
+          suffix: suffix,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(32))),
+          labelStyle: TextStyle(
+            color: Colors.black38,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+          labelText: label,
+          hintText: tip,
+        ),
+        keyboardType: textInputType,
+        validator: (value) {
+          if (mandatory == true) {
+            if (value.isEmpty) {
+              return 'Campo obrigatório';
+            }
+            return null;
+          }
+          return null;
+        });
+  }
+}
