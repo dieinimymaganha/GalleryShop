36f667c6554758b7389a9c182e3a6592be10af48
Dieinimy <dieinimy@gmail.com>
2020-08-06 20:56:58 -0300

    Incluido os controller vindo da store

--- frontend/galleryshop/lib/screens/employees/create_new_employee_screen.dart
+++ frontend/galleryshop/lib/screens/employees/create_new_employee_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:galleryshop/data/values.dart';
 import 'package:galleryshop/stores/employee_store.dart';
 import 'package:galleryshop/widgets/custom_form.dart';
-import 'package:galleryshop/widgets/custom_form_date.dart';
 import 'package:intl/intl.dart';
 
 class CreateNewEmployeeScreen extends StatefulWidget {
@@ -21,7 +20,7 @@ class _CreateNewEmployeeScreenState extends State<CreateNewEmployeeScreen> {
 
   List<dynamic> dataTypeEmployee = List();
 
-  DateTime _dateTime;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -31,107 +30,132 @@ class _CreateNewEmployeeScreenState extends State<CreateNewEmployeeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Cadastrar novo funcionário',
-        ),
-        centerTitle: true,
-        backgroundColor: colorAppbar,
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top: 10, left: 8, right: 8, bottom: 10),
-        child: ListView(
-          children: <Widget>[
-            Form(
-              child: Column(
-                children: <Widget>[
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite o Nome',
-                    label: 'Nome',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setName,
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite o Sobrenome',
-                    label: 'Sobrenome',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setLastName,
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite o Apelido',
-                    label: 'Apelido',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setNickName,
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    controller: _controllerDate,
-                    mandatory: true,
-                    tip: 'Digite a Data de nascimento',
-                    label: 'Data nascimento',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setBirthDate,
-                    ontap: () {
-                      select();
-                    },
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite o telefone',
-                    label: 'Telefone',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setPhoneNumber,
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite o RG',
-                    label: 'RG',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setRg,
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite o CPF',
-                    label: 'CPF',
-                    textInputType: TextInputType.text,
-                    obscure: false,
-                    onChanged: employeeStore.setCpf,
-                  ),
-                  SizedBox(height: space),
-                  CustomForm(
-                    mandatory: true,
-                    tip: 'Digite a taxa de comissão',
-                    label: 'Comissão',
-                    textInputType: TextInputType.number,
-                    obscure: false,
-                    onChanged: employeeStore.setComissionRate,
-                  ),
-                  SizedBox(height: space),
-                  Divider(
-                    color: Colors.black,
-                  ),
-                  Text(
-                    'Selecione as funções:',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Observer(
-                    builder: (_) {
-                      return employeeStore.loadingTypeEmployee
+    return Observer(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Cadastrar novo funcionário',
+            ),
+            centerTitle: true,
+            backgroundColor: colorAppbar,
+          ),
+          body: Container(
+            padding: EdgeInsets.only(top: 10, left: 8, right: 8, bottom: 10),
+            child: ListView(
+              children: <Widget>[
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      CustomForm(
+                        controller: employeeStore.controllerFieldName,
+                        mandatory: true,
+                        tip: 'Digite o Nome',
+                        label: 'Nome',
+                        textInputType: TextInputType.text,
+                        obscure: false,
+                        onChanged: employeeStore.setName,
+                      ),
+                      SizedBox(height: space),
+                      SizedBox(height: space),
+                      CustomForm(
+                        controller: employeeStore.controllerFieldLastName,
+                        mandatory: true,
+                        tip: 'Digite o Sobrenome',
+                        label: 'Sobrenome',
+                        textInputType: TextInputType.text,
+                        obscure: false,
+                        onChanged: employeeStore.setLastName,
+                      ),
+                      SizedBox(height: space),
+                      CustomForm(
+                        controller: employeeStore.controllerFieldNickName,
+                        mandatory: true,
+                        tip: 'Digite o Apelido',
+                        label: 'Apelido',
+                        textInputType: TextInputType.text,
+                        obscure: false,
+                        onChanged: employeeStore.setNickName,
+                      ),
+                      SizedBox(height: space),
+                      CustomForm(
+                        icon: Icon(Icons.event),
+                        controller: employeeStore.controllerFieldBirthDate,
+                        mandatory: true,
+                        tip: 'Digite a Data de nascimento',
+                        label: 'Data nascimento',
+                        textInputType: TextInputType.text,
+                        obscure: false,
+                        ontap: () {
+                          changeAlterBirthDate();
+                        },
+                        onChanged: employeeStore.setBirthDate,
+                      ),
+                      SizedBox(height: space),
+                      CustomForm(
+                        maxlengthField: 16,
+                        controller: employeeStore.controllerFieldPhoneNumber,
+                        icon: Icon(Icons.phone),
+                        mandatory: true,
+                        tip: '(000) 00000-0000',
+                        label: 'Telefone',
+                        textInputType: TextInputType.number,
+                        obscure: false,
+                        onChanged: employeeStore.setPhoneNumber,
+                      ),
+                      SizedBox(height: space),
+                      CustomForm(
+                        controller: employeeStore.controllerFieldRg,
+                        mandatory: true,
+                        tip: 'Digite o RG',
+                        label: 'Incluir ponto e dígito "00.000.000-0"',
+                        textInputType: TextInputType.text,
+                        obscure: false,
+                        onChanged: employeeStore.setRg,
+                      ),
+                      SizedBox(height: space),
+                      CustomForm(
+                        maxlengthField: 14,
+                        controller: employeeStore.controllerFieldCpf,
+                        mandatory: true,
+                        tip: 'Digite o CPF',
+                        label: '000.000.000-00',
+                        textInputType: TextInputType.text,
+                        obscure: false,
+                        onChanged: employeeStore.setCpf,
+                      ),
+                      SizedBox(height: space),
+                      CustomForm(
+                        controller: employeeStore.controllerFieldComissionRate,
+                        mandatory: true,
+                        tip: 'Digite a taxa de comissão',
+                        label: 'Comissão',
+                        textInputType: TextInputType.number,
+                        obscure: false,
+                        onChanged: employeeStore.setComissionRate,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Campo obrigatório';
+                          } else if (value.isNotEmpty) {
+                            try {
+                              int.parse(value);
+                            } on Exception catch (_) {
+                              return 'Somente números, sem dígitos';
+                            }
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: space),
+                      Divider(
+                        color: Colors.black,
+                      ),
+                      Text(
+                        'Selecione as funções:',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      employeeStore.loadingTypeEmployee
                           ? Column(
                               children:
                                   employeeStore.dataTypeEmployee.map((typeEm) {
@@ -154,72 +178,72 @@ class _CreateNewEmployeeScreenState extends State<CreateNewEmployeeScreen> {
                                 );
                               }).toList(),
                             )
-                          : CircularProgressIndicator();
-                    },
-                  ),
-                  Divider(
-                    color: Colors.black,
-                  ),
-                  SizedBox(height: space),
-                  Container(
-                    height: 60,
-                    alignment: Alignment.centerLeft,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        stops: [0.3, 1],
-                        colors: [
-                          Color(0XFF212121),
-                          Color(0XFF616161),
-                        ],
+                          : CircularProgressIndicator(),
+                      Divider(
+                        color: Colors.black,
                       ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50),
-                      ),
-                    ),
-                    child: SizedBox.expand(
-                        child: FlatButton(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text(
-                                  'Cadastrar',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 20),
-                                  textAlign: TextAlign.center,
+                      SizedBox(height: space),
+                      Container(
+                        height: 60,
+                        alignment: Alignment.centerLeft,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            stops: [0.3, 1],
+                            colors: [
+                              Color(0XFF212121),
+                              Color(0XFF616161),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(50),
+                          ),
+                        ),
+                        child: SizedBox.expand(
+                            child: FlatButton(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      'Cadastrar',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Container(
+                                      child: SizedBox(
+                                        child: Icon(Icons.send),
+                                        height: 28,
+                                        width: 28,
+                                      ),
+                                    )
+                                  ],
                                 ),
-                                Container(
-                                  child: SizedBox(
-                                    child: Icon(Icons.send),
-                                    height: 28,
-                                    width: 28,
-                                  ),
-                                )
-                              ],
-                            ),
-                            onPressed: () {
-                              print(employeeStore.dataTypeEmployee);
-                            })),
+                                onPressed: employeeStore.buttonSavePressed)),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
-  void select() {
+  void changeAlterBirthDate() {
     DatePicker.showDatePicker(context,
         locale: DateTimePickerLocale.pt_br,
+        maxDateTime: DateTime.now(),
         dateFormat: 'dd/MM/yyyy', onConfirm: (dateTime, selectedIndex) {
-      setState(() {
-        _controllerDate.text = DateFormat('dd/MM/yyyy').format(dateTime);
-      });
+      String birthDate = DateFormat('dd/MM/yyyy').format(dateTime);
+      employeeStore.setBirthDateController(birthDate);
+      employeeStore.setBirthDate(birthDate);
     },
         pickerTheme: DateTimePickerTheme(
           backgroundColor: colorAppbar,
