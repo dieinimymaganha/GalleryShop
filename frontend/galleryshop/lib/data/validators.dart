73080767f22a967a73fbcb0ec9d2e33f48f1538b
Dieinimy <dieinimy@gmail.com>
2020-08-09 17:54:53 -0300

    Criado validators para validar formulario

---
+++ frontend/galleryshop/lib/data/validators.dart
@@ -0,0 +1,55 @@
+String validatorComissionRate(String value) {
+  if (value.isEmpty) {
+    return 'Campo obrigatório';
+  } else if (value.isNotEmpty) {
+    try {
+      int.parse(value);
+    } on Exception catch (_) {
+      return 'Somente números, sem dígitos';
+    }
+  }
+  return null;
+}
+
+String validatorName(String value) {
+  if (value.isEmpty) {
+    return 'Campo obrigatório';
+  }
+}
+
+String validatorCpf(String value) {
+  const Pattern pattern = r'\d{3}\.\d{3}\.\d{3}\-\d{2}';
+  final RegExp regex = RegExp(pattern);
+  if (value.isEmpty) {
+    return 'Campo obrigatório';
+  } else if (!regex.hasMatch(value)) {
+    return 'CPF inválido';
+  }
+  return null;
+}
+
+String validatorBirthDate(String value) {
+  const Pattern pattern =
+      r'(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}';
+  final RegExp regex = RegExp(pattern);
+  if (value.isEmpty) {
+    return 'Campo obrigatório';
+  } else if (!regex.hasMatch(value)) {
+    return 'Data inválida';
+  }
+  return null;
+}
+
+String validatorPhoneNumber(String value){
+  const Pattern pattern =
+      r'(\(\d{3}\)\s)(\d{4,5}\-\d{4})';
+  final RegExp regex = RegExp(pattern);
+  if (value.isEmpty) {
+    return 'Campo obrigatório';
+  } else if (!regex.hasMatch(value)) {
+    return 'Telefone inválido';
+  }
+  return null;
+}
+
+
