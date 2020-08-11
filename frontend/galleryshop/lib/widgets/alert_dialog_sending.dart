2bedb56a3433b12726b3a65954643fcacbf3a377
Dieinimy <dieinimy@gmail.com>
2020-08-11 19:57:56 -0300

    Criado AlertDiagloSending.

---
+++ frontend/galleryshop/lib/widgets/alert_dialog_sending.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:galleryshop/data/values.dart';
+
+class AlertDialogSending extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: colorAppbar,
+      content: LinearProgressIndicator(
+        backgroundColor: Colors.white10,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60.0)),
+    );
+  }
+}
