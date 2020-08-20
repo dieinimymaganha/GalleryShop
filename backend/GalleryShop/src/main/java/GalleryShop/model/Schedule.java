9be96513bd6aac87eaebc59d5dad75e148207f29
Dieinimy <dieinimy@gmail.com>
2020-08-19 21:55:51 -0300

    Removido bibliotecas não utilizadas

--- backend/GalleryShop/src/main/java/GalleryShop/model/Schedule.java
+++ backend/GalleryShop/src/main/java/GalleryShop/model/Schedule.java
@@ -3,21 +3,14 @@ package GalleryShop.model;
 import java.time.LocalTime;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
 
 @Entity
 public class Schedule {
