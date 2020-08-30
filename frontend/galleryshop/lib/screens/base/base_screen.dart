fccb3bf77ceb2096dc857c12047dddd695bdb69b
Dieinimy <dieinimy@gmail.com>
2020-08-30 09:58:20 -0300

    Alterado caminho para tela de base da agenda

--- frontend/galleryshop/lib/screens/base/base_screen.dart
+++ frontend/galleryshop/lib/screens/base/base_screen.dart
@@ -11,6 +11,7 @@ import 'package:galleryshop/screens/employees/employees_base_screen.dart';
 import 'package:galleryshop/screens/home/home_screen.dart';
 import 'package:galleryshop/screens/my_account/my_account_screen.dart';
 import 'package:galleryshop/screens/reports/reports_screen.dart';
+import 'package:galleryshop/screens/schedule/schedule_base_screen.dart';
 import 'package:galleryshop/screens/schedule/schedule_screen.dart';
 import 'package:galleryshop/screens/services/services_list_screen.dart';
 import 'package:galleryshop/screens/stock/stock_screen.dart';
@@ -61,7 +62,7 @@ class _BaseScreenState extends State<BaseScreen> {
           HomeScreen(),
           ClientListScreen(),
           EmployeeBaseScreen(),
-          ScheduleScreen(),
+          ScheduleBaseScreen(),
           AccountsScreen(),
           MyAccountScreen(),
           BillsToPayScreen(),
