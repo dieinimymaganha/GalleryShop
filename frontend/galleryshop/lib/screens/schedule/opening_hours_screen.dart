fefa165e6dc7e921290ac2581b37726fa45b2f42
Dieinimy <dieinimy@gmail.com>
2020-08-30 12:46:02 -0300

    Só recupCriaderado o id do funcionário

--- frontend/galleryshop/lib/screens/schedule/opening_hours_screen.dart
+++ frontend/galleryshop/lib/screens/schedule/opening_hours_screen.dart
@@ -1,60 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:galleryshop/data/values.dart';
 import 'package:galleryshop/screens/base/base_screen.dart';
-import 'package:galleryshop/widgets/custom_form.dart';
+import 'package:galleryshop/stores/opening_hours_store.dart';
 
-class OpeningHoursScreen extends StatelessWidget {
+class OpeningHoursScreen extends StatefulWidget {
+  @override
+  _OpeningHoursScreenState createState() => _OpeningHoursScreenState();
+}
+
+class _OpeningHoursScreenState extends State<OpeningHoursScreen> {
+  OpeningHoursStore openingHoursStore = OpeningHoursStore();
+
+  @override
+  void initState() {
+    super.initState();
+    openingHoursStore.setIdEmployee();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Horario de atendimento'),
-        centerTitle: true,
-        backgroundColor: colorAppbar,
-        leading: IconButton(
-          icon: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
+        appBar: AppBar(
+          title: Text('Horario de atendimento'),
+          centerTitle: true,
+          backgroundColor: colorAppbar,
+          leading: IconButton(
+            icon: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => BaseScreen()));
+              },
             ),
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => BaseScreen()));
-            },
           ),
         ),
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top: 10, bottom: 10, right: 5, left: 5),
-        child: ListView(
-          children: <Widget>[
-            GestureDetector(
-              onLongPress: (){},
-              child: Card(
-                shape: (RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
-                color: colorCard,
-                child: ExpansionTile(
-                  title: Text('Segunda', style: TextStyle(fontSize: 20),),
-                  children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Divider(
-                          color: Colors.white,
-                          thickness: 2,
-                        ),
-                        TextFormField()
-                      ],
-                    )
-                  ],
-
-                ),
-              ),
-            )
-          ],
-        ),
-      )
-    );
+        body: Container(
+          padding: EdgeInsets.only(top: 10, bottom: 10, right: 5, left: 5),
+          child: Observer(
+            builder: ((_) {
+              return ListView(
+                children: <Widget>[
+                  Text(openingHoursStore.idEmployee.toString()),
+                ],
+              );
+            }),
+          ),
+        ));
   }
 }
