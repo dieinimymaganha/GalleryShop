4469272b91117f8633eeb84745005c4fb02f5f70
Dieinimy <dieinimy@gmail.com>
2020-08-30 11:32:10 -0300

    Iniciada alteração do customDrawerHeader para exibir o nome inicial do funcionário.

--- frontend/galleryshop/lib/widgets/drawer/custom_header.dart
+++ frontend/galleryshop/lib/widgets/drawer/custom_header.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:galleryshop/stores/my_account_store.dart';
 
-class CustomHeard extends StatelessWidget {
+class CustomHeard extends StatefulWidget {
+  @override
+  _CustomHeardState createState() => _CustomHeardState();
+}
+
+class _CustomHeardState extends State<CustomHeard> {
+  MyAccountStore myAccountStore = MyAccountStore();
+
+  @override
+  void initState() {
+    super.initState();
+    myAccountStore.setPhoneNumberLogin();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-      color: Colors.black54,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Observer(
+      builder: (_) {
+        return Container(
+          height: 100,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          color: Colors.black54,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Text('Bem Vindo Dieinimy!',style: TextStyle(color: Colors.white, fontSize: 15),),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    myAccountStore.phoneNumberLogin,
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
     );
   }
 }
