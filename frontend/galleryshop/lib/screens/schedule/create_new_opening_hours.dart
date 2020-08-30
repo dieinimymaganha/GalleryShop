5959ea520c588a1f0bacf44e342d3e077a6b3b4f
Dieinimy <dieinimy@gmail.com>
2020-08-30 15:54:50 -0300

    Iniciada a criação da tela para cadastrar os horarios.

---
+++ frontend/galleryshop/lib/screens/schedule/create_new_opening_hours.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CreateNewOpeningHours extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastrar novo horario'),
+      ),
+    );
+  }
+}
