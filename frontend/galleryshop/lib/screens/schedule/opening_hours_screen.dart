d716a6169d4a3c2e556219c199b7f8d4a727a690
Dieinimy <dieinimy@gmail.com>
2020-08-30 15:25:00 -0300

    Criada primeira versão da tela de horarios

--- frontend/galleryshop/lib/screens/schedule/opening_hours_screen.dart
+++ frontend/galleryshop/lib/screens/schedule/opening_hours_screen.dart
@@ -39,13 +39,106 @@ class _OpeningHoursScreenState extends State<OpeningHoursScreen> {
           ),
         ),
         body: Container(
-          padding: EdgeInsets.only(top: 10, bottom: 10, right: 5, left: 5),
+          padding: EdgeInsets.only(top: 10, bottom: 10),
           child: Observer(
             builder: ((_) {
               return ListView(
-                children: <Widget>[
-                  Text(openingHoursStore.idEmployee.toString()),
-                ],
+                children:
+                    openingHoursStore.listOpeningHours.map((openingHoursModel) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80),
+                    child: InkWell(
+                      onDoubleTap: () {},
+                      onLongPress: () {},
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80.0),
+                        ),
+                        color: colorCard,
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                        left: 15,
+                                        right: 15,
+                                        top: 10,
+                                        bottom: 10),
+                                    child: Column(
+                                      children: <Widget>[
+                                        Text(
+                                          openingHoursModel.dayOfTheWeek ==
+                                                  'TERCA'
+                                              ? 'TERÇA'
+                                              : openingHoursModel
+                                                          .dayOfTheWeek ==
+                                                      'SABADO'
+                                                  ? 'SÁBADO'
+                                                  : openingHoursModel
+                                                      .dayOfTheWeek,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w800,
+                                              fontSize: 18),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Column(
+                                          children: <Widget>[
+                                            Text(
+                                              'Horario manhã:',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            Text(openingHoursModel
+                                                .earlyMorningJourney),
+                                            Text(openingHoursModel
+                                                .endMorningJourney),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                          ],
+                                        ),
+                                        openingHoursModel
+                                                    .earlyAfternoonJourney ==
+                                                null
+                                            ? Container()
+                                            : Column(
+                                                children: <Widget>[
+                                                  Text(
+                                                    'Horario tarde:',
+                                                    style: TextStyle(
+                                                        fontSize: 15,
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                  Text(openingHoursModel
+                                                      .earlyAfternoonJourney)
+                                                ],
+                                              ),
+                                        openingHoursModel.endJourneyLate == null
+                                            ? Container()
+                                            : Column(
+                                                children: <Widget>[
+                                                  Text(openingHoursModel
+                                                      .endJourneyLate)
+                                                ],
+                                              ),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
               );
             }),
           ),
