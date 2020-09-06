85a853f0b7a8b1aaafe87fba04e75e5cad8c4409
Dieinimy <dieinimy@gmail.com>
2020-09-06 18:19:28 -0300

    Realizada a criação dos controller e alterações para tipo datetime para poder realizar as comparações de datas

--- frontend/galleryshop/lib/stores/opening_hours_store.dart
+++ frontend/galleryshop/lib/stores/opening_hours_store.dart
@@ -1,16 +1,106 @@
+import 'package:date_format/date_format.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:galleryshop/http/webclients/webclient_opening_hours.dart';
+import 'package:galleryshop/models/opening_hours.dart';
+import 'package:intl/intl.dart';
 import 'package:mobx/mobx.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 part 'opening_hours_store.g.dart';
 
+class DayOfTheWeek {
+  String description;
+  String day;
+
+  DayOfTheWeek({this.description, this.day});
+}
+
 class OpeningHoursStore = _OpeningHoursStore with _$OpeningHoursStore;
 
 abstract class _OpeningHoursStore with Store {
+  final OpeninigHoursDto openinigHoursDto;
+
+  _OpeningHoursStore({this.openinigHoursDto}) {
+    autorun((_) {
+      print('morningStart >>> $morningStart');
+      print('morningEnd >>> $morningEnd');
+      print('afternoonStart >>> $afternoonStart');
+      print('afternoonEnd >>> $afternoonEnd');
+    });
+  }
+
   OpeningHoursWebClient openingHoursWebClient = OpeningHoursWebClient();
 
   @observable
+  List<DayOfTheWeek> listDayOfWeek = [
+    new DayOfTheWeek(description: 'SEGUNDA', day: 'SEGUNDA'),
+    new DayOfTheWeek(description: 'TERCA', day: 'TERÇA'),
+    new DayOfTheWeek(description: 'QUARTA', day: 'QUARTA'),
+    new DayOfTheWeek(description: 'QUINTA', day: 'QUINTA'),
+    new DayOfTheWeek(description: 'SEXTA', day: 'SEXTA'),
+    new DayOfTheWeek(description: 'SABADO', day: 'SÁBADO'),
+    new DayOfTheWeek(description: 'DOMINGO', day: 'DOMINGO'),
+  ];
+
+  @observable
   int idEmployee = 0;
+
+  @observable
+  String valueSelect;
+
+  @observable
+  DateTime morningStart;
+
+  @computed
+  bool get fieldsValid => morningStart != null && morningEnd != null;
+
+  @action
+  void setMoringStart(TextEditingController controller) {
+    DateTime dateConvert = DateFormat("hh:mm").parse(controller.text);
+    morningStart = dateConvert;
+    fieldsValid;
+  }
+
+  @action
+  void setMoringEnd(TextEditingController controller) {
+    DateTime dateConvert = DateFormat("hh:mm").parse(controller.text);
+    morningEnd = dateConvert;
+    fieldsValid;
+  }
+
+  @action
+  void setAfternoonStart(TextEditingController controller) {
+    DateTime dateConvert = DateFormat("hh:mm").parse(controller.text);
+    afternoonStart = dateConvert;
+    fieldsValid;
+  }
+
+  @action
+  void setAfternoonEnd(TextEditingController controller) {
+    DateTime dateConvert = DateFormat("hh:mm").parse(controller.text);
+    afternoonEnd = dateConvert;
+    fieldsValid;
+  }
+
+  TextEditingController controlerMorningStart = TextEditingController();
+
+  @observable
+  DateTime morningEnd;
+
+  TextEditingController controlerMorningEnd = TextEditingController();
+
+  @observable
+  DateTime afternoonStart;
+
+  TextEditingController controlerAfternoonStart = TextEditingController();
+
+  @observable
+  DateTime afternoonEnd;
+
+  TextEditingController controlerAfternoonEnd = TextEditingController();
+
+  @action
+  void selectDayOfTheWeek(String value) => valueSelect = value;
 
   @observable
   List<dynamic> listOpeningHours = List();
--- frontend/galleryshop/lib/stores/opening_hours_store.g.dart
+++ frontend/galleryshop/lib/stores/opening_hours_store.g.dart
@@ -9,6 +9,29 @@ part of 'opening_hours_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$OpeningHoursStore on _OpeningHoursStore, Store {
+  Computed<bool> _$fieldsValidComputed;
+
+  @override
+  bool get fieldsValid =>
+      (_$fieldsValidComputed ??= Computed<bool>(() => super.fieldsValid,
+              name: '_OpeningHoursStore.fieldsValid'))
+          .value;
+
+  final _$listDayOfWeekAtom = Atom(name: '_OpeningHoursStore.listDayOfWeek');
+
+  @override
+  List<DayOfTheWeek> get listDayOfWeek {
+    _$listDayOfWeekAtom.reportRead();
+    return super.listDayOfWeek;
+  }
+
+  @override
+  set listDayOfWeek(List<DayOfTheWeek> value) {
+    _$listDayOfWeekAtom.reportWrite(value, super.listDayOfWeek, () {
+      super.listDayOfWeek = value;
+    });
+  }
+
   final _$idEmployeeAtom = Atom(name: '_OpeningHoursStore.idEmployee');
 
   @override
@@ -21,6 +44,81 @@ mixin _$OpeningHoursStore on _OpeningHoursStore, Store {
   set idEmployee(int value) {
     _$idEmployeeAtom.reportWrite(value, super.idEmployee, () {
       super.idEmployee = value;
+    });
+  }
+
+  final _$valueSelectAtom = Atom(name: '_OpeningHoursStore.valueSelect');
+
+  @override
+  String get valueSelect {
+    _$valueSelectAtom.reportRead();
+    return super.valueSelect;
+  }
+
+  @override
+  set valueSelect(String value) {
+    _$valueSelectAtom.reportWrite(value, super.valueSelect, () {
+      super.valueSelect = value;
+    });
+  }
+
+  final _$morningStartAtom = Atom(name: '_OpeningHoursStore.morningStart');
+
+  @override
+  DateTime get morningStart {
+    _$morningStartAtom.reportRead();
+    return super.morningStart;
+  }
+
+  @override
+  set morningStart(DateTime value) {
+    _$morningStartAtom.reportWrite(value, super.morningStart, () {
+      super.morningStart = value;
+    });
+  }
+
+  final _$morningEndAtom = Atom(name: '_OpeningHoursStore.morningEnd');
+
+  @override
+  DateTime get morningEnd {
+    _$morningEndAtom.reportRead();
+    return super.morningEnd;
+  }
+
+  @override
+  set morningEnd(DateTime value) {
+    _$morningEndAtom.reportWrite(value, super.morningEnd, () {
+      super.morningEnd = value;
+    });
+  }
+
+  final _$afternoonStartAtom = Atom(name: '_OpeningHoursStore.afternoonStart');
+
+  @override
+  DateTime get afternoonStart {
+    _$afternoonStartAtom.reportRead();
+    return super.afternoonStart;
+  }
+
+  @override
+  set afternoonStart(DateTime value) {
+    _$afternoonStartAtom.reportWrite(value, super.afternoonStart, () {
+      super.afternoonStart = value;
+    });
+  }
+
+  final _$afternoonEndAtom = Atom(name: '_OpeningHoursStore.afternoonEnd');
+
+  @override
+  DateTime get afternoonEnd {
+    _$afternoonEndAtom.reportRead();
+    return super.afternoonEnd;
+  }
+
+  @override
+  set afternoonEnd(DateTime value) {
+    _$afternoonEndAtom.reportWrite(value, super.afternoonEnd, () {
+      super.afternoonEnd = value;
     });
   }
 
@@ -63,11 +161,76 @@ mixin _$OpeningHoursStore on _OpeningHoursStore, Store {
     return _$getIdEmployeeAsyncAction.run(() => super.getIdEmployee());
   }
 
+  final _$_OpeningHoursStoreActionController =
+      ActionController(name: '_OpeningHoursStore');
+
+  @override
+  void setMoringStart(TextEditingController controller) {
+    final _$actionInfo = _$_OpeningHoursStoreActionController.startAction(
+        name: '_OpeningHoursStore.setMoringStart');
+    try {
+      return super.setMoringStart(controller);
+    } finally {
+      _$_OpeningHoursStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setMoringEnd(TextEditingController controller) {
+    final _$actionInfo = _$_OpeningHoursStoreActionController.startAction(
+        name: '_OpeningHoursStore.setMoringEnd');
+    try {
+      return super.setMoringEnd(controller);
+    } finally {
+      _$_OpeningHoursStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setAfternoonStart(TextEditingController controller) {
+    final _$actionInfo = _$_OpeningHoursStoreActionController.startAction(
+        name: '_OpeningHoursStore.setAfternoonStart');
+    try {
+      return super.setAfternoonStart(controller);
+    } finally {
+      _$_OpeningHoursStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setAfternoonEnd(TextEditingController controller) {
+    final _$actionInfo = _$_OpeningHoursStoreActionController.startAction(
+        name: '_OpeningHoursStore.setAfternoonEnd');
+    try {
+      return super.setAfternoonEnd(controller);
+    } finally {
+      _$_OpeningHoursStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void selectDayOfTheWeek(String value) {
+    final _$actionInfo = _$_OpeningHoursStoreActionController.startAction(
+        name: '_OpeningHoursStore.selectDayOfTheWeek');
+    try {
+      return super.selectDayOfTheWeek(value);
+    } finally {
+      _$_OpeningHoursStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
+listDayOfWeek: ${listDayOfWeek},
 idEmployee: ${idEmployee},
-listOpeningHours: ${listOpeningHours}
+valueSelect: ${valueSelect},
+morningStart: ${morningStart},
+morningEnd: ${morningEnd},
+afternoonStart: ${afternoonStart},
+afternoonEnd: ${afternoonEnd},
+listOpeningHours: ${listOpeningHours},
+fieldsValid: ${fieldsValid}
     ''';
   }
 }
