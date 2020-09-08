2a27469ad16211a1fbcce5c0a6600a6ed959949f
Dieinimy <dieinimy@gmail.com>
2020-09-08 11:08:00 -0300

    Incluido observer na tela inteira

--- frontend/galleryshop/lib/screens/schedule/opening_hours_screen.dart
+++ frontend/galleryshop/lib/screens/schedule/opening_hours_screen.dart
@@ -21,84 +21,97 @@ class _OpeningHoursScreenState extends State<OpeningHoursScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Horario de atendimento'),
-          centerTitle: true,
-          backgroundColor: colorAppbar,
-          leading: IconButton(
-            icon: IconButton(
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
+    return Observer(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Horario de atendimento'),
+            centerTitle: true,
+            backgroundColor: colorAppbar,
+            leading: IconButton(
+              icon: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => BaseScreen()));
+                },
               ),
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => BaseScreen()));
-              },
             ),
           ),
-        ),
-        body: Container(
-          child: Observer(
-            builder: ((_) {
-              return ListView(
-                children:
-                    openingHoursStore.listOpeningHours.map((openingHoursModel) {
-                  return Container(
-                    padding: EdgeInsets.only(top: 10, left: 8, right: 8),
-                    child: Column(
-                      children: <Widget>[
-                        InkWell(
-                          onDoubleTap: () {},
-                          onLongPress: () {},
-                          child: ListTile(
-                            title: Text(
-                              openingHoursModel.dayOfTheWeek == 'TERCA'
-                                  ? 'TERÇA'
-                                  : openingHoursModel.dayOfTheWeek == 'SABADO'
-                                      ? 'SÁBADO'
-                                      : openingHoursModel.dayOfTheWeek, style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w500),
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Text('Horario atendimento: ', style: TextStyle(color: Colors.black87),),
-                                    Text(openingHoursModel.endJourneyLate ==
-                                            null
-                                        ? '${openingHoursModel.earlyMorningJourney} ás ${openingHoursModel.endMorningJourney}'
-                                        : '${openingHoursModel.earlyMorningJourney} ás ${openingHoursModel.endJourneyLate}')
-                                  ],
-                                ),
-                                openingHoursModel.endJourneyLate == null
-                                    ? Container()
-                                    : Row(
-                                        children: <Widget>[
-                                          Text('Horario almoço: ', style: TextStyle(color: Colors.black87),),
-                                          Text(
-                                              '${openingHoursModel.endMorningJourney} ás ${openingHoursModel.earlyAfternoonJourney}')
-                                        ],
+          body: Container(
+            child: Observer(
+              builder: ((_) {
+                return ListView(
+                  children: openingHoursStore.listOpeningHours
+                      .map((openingHoursModel) {
+                    return Container(
+                      padding: EdgeInsets.only(top: 10, left: 8, right: 8),
+                      child: Column(
+                        children: <Widget>[
+                          InkWell(
+                            onDoubleTap: () {},
+                            onLongPress: () {},
+                            child: ListTile(
+                              title: Text(
+                                openingHoursModel.dayOfTheWeek == 'TERCA'
+                                    ? 'TERÇA'
+                                    : openingHoursModel.dayOfTheWeek == 'SABADO'
+                                        ? 'SÁBADO'
+                                        : openingHoursModel.dayOfTheWeek,
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        'Horario atendimento: ',
+                                        style: TextStyle(color: Colors.black87),
                                       ),
-                              ],
+                                      Text(openingHoursModel.endJourneyLate ==
+                                              null
+                                          ? '${openingHoursModel.earlyMorningJourney} ás ${openingHoursModel.endMorningJourney}'
+                                          : '${openingHoursModel.earlyMorningJourney} ás ${openingHoursModel.endJourneyLate}')
+                                    ],
+                                  ),
+                                  openingHoursModel.endJourneyLate == null
+                                      ? Container()
+                                      : Row(
+                                          children: <Widget>[
+                                            Text(
+                                              'Horario almoço: ',
+                                              style: TextStyle(
+                                                  color: Colors.black87),
+                                            ),
+                                            Text(
+                                                '${openingHoursModel.endMorningJourney} ás ${openingHoursModel.earlyAfternoonJourney}')
+                                          ],
+                                        ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        Divider(
-                          color: Colors.grey[400],
-                          thickness: 0.5,
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-              );
-            }),
-
+                          Divider(
+                            color: Colors.grey[400],
+                            thickness: 0.5,
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              }),
+            ),
           ),
-
-        ),
-    floatingActionButton: ButtonCreateNewOpeningHours(),);
+          floatingActionButton: ButtonCreateNewOpeningHours(),
+        );
+      },
+    );
   }
 }
