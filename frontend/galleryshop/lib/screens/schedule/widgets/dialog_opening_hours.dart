94a6b1604910ca2c3df102bd3c40114f076b08f6
Dieinimy <dieinimy@gmail.com>
2020-09-08 12:53:43 -0300

    Criado Dialog para excluir horário de atendimento

---
+++ frontend/galleryshop/lib/screens/schedule/widgets/dialog_opening_hours.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:galleryshop/models/opening_hours.dart';
+import 'package:galleryshop/screens/schedule/opening_hours_screen.dart';
+import 'package:galleryshop/stores/opening_hours_store.dart';
+import 'package:galleryshop/widgets/alert_dialog_sending.dart';
+import 'package:galleryshop/widgets/custom_alert_dialog.dart';
+import 'package:mobx/mobx.dart';
+
+class DialogOpeningHours extends StatefulWidget {
+  final OpeninigHoursDto openinigHoursDto;
+
+  DialogOpeningHours({this.openinigHoursDto});
+
+  @override
+  _DialogOpeningHoursState createState() =>
+      _DialogOpeningHoursState(openinigHoursDto: openinigHoursDto);
+}
+
+class _DialogOpeningHoursState extends State<DialogOpeningHours> {
+  OpeningHoursStore openingHoursStore = OpeningHoursStore();
+
+  _DialogOpeningHoursState({OpeninigHoursDto openinigHoursDto})
+      : openingHoursStore =
+            OpeningHoursStore(openinigHoursDto: openinigHoursDto);
+
+  ReactionDisposer disposer;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    disposer = reaction((_) => openingHoursStore.excluded, (excluded) async {
+      if (excluded) {
+        showDialog(
+            context: context, builder: (context) => buildAlertDialogOK());
+        await Future.delayed(Duration(seconds: 2));
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => OpeningHoursScreen()));
+      }
+    });
+
+    disposer =
+        reaction((_) => openingHoursStore.excludedFail, (excludedFail) async {
+          if (excludedFail) {
+            showDialog(
+                context: context, builder: (context) => buildAlertDialogError());
+            await Future.delayed(Duration(seconds: 2));
+            Navigator.of(context).pop();
+          }
+        });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) {
+        return openingHoursStore.sending
+            ? AlertDialogSending()
+            : buildAlertDialog();
+      },
+    );
+  }
+
+  CustomAlertDialog buildAlertDialogOK() {
+    return CustomAlertDialog(
+      icon: Icon(
+        Icons.message,
+        color: Colors.white,
+      ),
+      message: 'Excluido com sucesso!',
+      color: Colors.blueAccent,
+    );
+  }
+
+  CustomAlertDialog buildAlertDialogError() {
+    return CustomAlertDialog(
+      icon: Icon(
+        Icons.error,
+        color: Colors.white,
+      ),
+      message: 'Falha ao excluir!',
+      color: Colors.redAccent,
+    );
+  }
+
+  AlertDialog buildAlertDialog() {
+    return AlertDialog(
+      title: Text(
+        'Excluir',
+        style: TextStyle(color: Colors.red),
+      ),
+      content: Text(
+        'Dia da semana: ${widget.openinigHoursDto.dayOfTheWeek == 'SABADO' ? 'SÁBADO' : widget.openinigHoursDto.dayOfTheWeek == 'TERCA' ? 'TERÇA' : widget.openinigHoursDto.dayOfTheWeek}',
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: openingHoursStore.buttoExcludePressed,
+          child: Text('Sim'),
+        ),
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Não',
+            style: TextStyle(color: Colors.blueGrey),
+          ),
+        ),
+      ],
+      elevation: 24.0,
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    );
+  }
+}
