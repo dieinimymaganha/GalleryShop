997bdfbbe5a56b9cb19a340372a17a1453207845
Dieinimy <dieinimy@gmail.com>
2020-09-13 16:35:12 -0300

    Criado tela minha agenda

---
+++ frontend/galleryshop/lib/screens/schedule/schedule_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/stores/schedule_store.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class ScheduleScreen extends StatefulWidget {
+  @override
+  _ScheduleScreenState createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  ScheduleStore scheduleStore = ScheduleStore();
+
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+    scheduleStore.setListSchedule();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha agenda'),
+        backgroundColor: colorAppbar,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: TableCalendar(
+          locale: 'pt_BR',
+          calendarController: _controller,
+          initialCalendarFormat: CalendarFormat.week,
+          formatAnimation: FormatAnimation.slide,
+          startingDayOfWeek: StartingDayOfWeek.monday,
+          calendarStyle: CalendarStyle(selectedColor: Colors.blueGrey),
+        ),
+      ),
+    );
+  }
+}
