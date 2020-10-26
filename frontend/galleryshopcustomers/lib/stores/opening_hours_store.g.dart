2539764ba0953032b71088352f92237d752242e7
Dieinimy <dieinimy@gmail.com>
2020-10-26 17:36:48 -0300

    Atualizado stores - Clientes

--- frontend/galleryshopcustomers/lib/stores/opening_hours_store.g.dart
+++ frontend/galleryshopcustomers/lib/stores/opening_hours_store.g.dart
@@ -6,7 +6,7 @@ part of 'opening_hours_store.dart';
 // StoreGenerator
 // **************************************************************************
 
-// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$OpeningHoursStore on _OpeningHoursStore, Store {
   Computed<Function> _$buttoExcludePressedComputed;
