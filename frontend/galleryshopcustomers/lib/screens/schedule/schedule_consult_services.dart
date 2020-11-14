50b00624230e4b9b87abefd9821893a796c2e881
Dieinimy <dieinimy@gmail.com>
2020-11-14 14:38:49 -0300

    Iniciada criação da tela para consultar as agendas

---
+++ frontend/galleryshopcustomers/lib/screens/schedule/schedule_consult_services.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:galleryshopcustomers/stores/schedule_store.dart';
+
+class ScheduleConsultServices extends StatefulWidget {
+  @override
+  _ScheduleConsultServicesState createState() =>
+      _ScheduleConsultServicesState();
+}
+
+class _ScheduleConsultServicesState extends State<ScheduleConsultServices> {
+  ScheduleStore scheduleStore = ScheduleStore();
+
+  @override
+  void initState() {
+    super.initState();
+    scheduleStore.getServices();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Consultar'),
+            centerTitle: true,
+          ),
+          body: Container(
+            padding: EdgeInsets.only(top: 10, left: 8, right: 8),
+            color: Colors.white,
+            child: ListView(
+              children: <Widget>[
+                DropdownButton(
+                  hint: Text('Tipo de funcionário'),
+                  value: scheduleStore.valueSelect,
+                  items: scheduleStore.dataServices.map((item) {
+                    return DropdownMenuItem(
+                      child: Text(item.description.toString()),
+                      value: item.description.toString(),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    scheduleStore.selectTypeService(value);
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
