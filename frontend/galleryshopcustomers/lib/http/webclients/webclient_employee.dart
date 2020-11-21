1bc11df147a6d2481c6e9efb39ba95e49b3beaee
Dieinimy <dieinimy@gmail.com>
2020-11-21 11:18:00 -0300

    criado função para coletar os dados do endpoint de identificação de funcionários por tipo

--- frontend/galleryshopcustomers/lib/http/webclients/webclient_employee.dart
+++ frontend/galleryshopcustomers/lib/http/webclients/webclient_employee.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-
 import 'package:galleryshopcustomers/data/function_generic.dart';
 import 'package:galleryshopcustomers/models/employee.dart';
 import 'package:http/http.dart';
@@ -25,7 +24,7 @@ class EmployeeWebClient {
     if (response.statusCode == 200) {
       final List<dynamic> decodeJson = jsonDecode(response.body);
       final List<dynamic> data =
-          decodeJson.map((dynamic json) => EmployeeDto.fromJson(json)).toList();
+      decodeJson.map((dynamic json) => EmployeeDto.fromJson(json)).toList();
       return data;
     }
     throw HttpException(_getMessage(response.statusCode));
@@ -43,6 +42,25 @@ class EmployeeWebClient {
 
     if (response.statusCode == 200) {
       final EmployeeDto data = EmployeeDto.fromJson(jsonDecode(response.body));
+      return data;
+    }
+    throw HttpException(_getMessage(response.statusCode));
+  }
+
+  Future<List<EmployeeDto>> findEmployeeTypeEmployee(int id) async {
+    String token = await getToken();
+    final Response response = await webClient.get(
+      urlEmployee + "/typeEmployeeId=" + id.toString(),
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': "Bearer $token"
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> decodeJson = jsonDecode(response.body);
+      final List<dynamic> data =
+      decodeJson.map((dynamic json) => EmployeeDto.fromJson(json)).toList();
       return data;
     }
     throw HttpException(_getMessage(response.statusCode));
