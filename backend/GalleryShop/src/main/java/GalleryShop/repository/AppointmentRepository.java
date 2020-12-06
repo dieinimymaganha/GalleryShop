60d23501b6e04ce85c8aa52b1b8fdc353f254ccf
Dieinimy <dieinimy@gmail.com>
2020-12-06 12:07:08 -0300

    Removido modelo não utilizado

--- backend/GalleryShop/src/main/java/GalleryShop/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package GalleryShop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import GalleryShop.model.Appointment;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
-    
-}
