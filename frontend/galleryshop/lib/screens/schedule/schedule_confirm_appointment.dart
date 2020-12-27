4fe8ca73ef010eb93f2c912c75d5cffa162357e4
Dieinimy <dieinimy@gmail.com>
2020-12-27 10:33:51 -0300

    Realizada alteração do visual do campo de pesquisa

--- frontend/galleryshop/lib/screens/schedule/schedule_confirm_appointment.dart
+++ frontend/galleryshop/lib/screens/schedule/schedule_confirm_appointment.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:galleryshop/data/function_generic.dart';
 import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/screens/client/widget/button_create_client.dart';
 import 'package:galleryshop/stores/client_store.dart';
 
 class ScheduleConfirmAppointment extends StatefulWidget {
-  final List<String> list = List.generate(10, (index) => "Texto $index");
-
   @override
   _ScheduleConfirmAppointmentState createState() =>
       _ScheduleConfirmAppointmentState();
@@ -27,159 +27,145 @@ class _ScheduleConfirmAppointmentState
     return Observer(
       builder: (_) {
         return Scaffold(
-            appBar: AppBar(
-              backgroundColor: colorAppbar,
-              title: TextField(
-                onChanged: clientStore.setFilter,
-                decoration: InputDecoration(hintText: 'Pesquisar'),
+          appBar: AppBar(
+            backgroundColor: colorAppbar,
+            title: TextFormField(
+//              style: TextStyle(color: Colors.white),
+              onChanged: clientStore.setFilter,
+              decoration: InputDecoration(
+                
+                suffixIcon: Icon(Icons.search),
+                hintText: 'Pesquisar...',
+                filled: true,
+                fillColor: Colors.grey[200],
               ),
             ),
-            body: ListView(
-              children: clientStore.lisFiltered.map((clientModel) {
-                return Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5,),
-                  child: InkWell(
-                    child: Card(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                          borderRadius:
-                          BorderRadius.circular(20.0)),
-                      color: colorCard,
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.only(
-                                      left: 15,
-                                      right: 15,
-                                      top: 10,
-                                      bottom: 10),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                    CrossAxisAlignment.start,
-                                    mainAxisAlignment:
-                                    MainAxisAlignment
-                                        .spaceBetween,
-                                    children: <Widget>[
-                                      Text(
-                                        clientModel.nickname,
-                                        style: TextStyle(
-                                            fontWeight:
-                                            FontWeight.w700,
-                                            fontSize: 18),
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text(
-                                            'Nome: ',
-                                            style: TextStyle(
-                                                fontWeight:
-                                                FontWeight
-                                                    .w700),
+          ),
+          body: ListView(
+            children: clientStore.lisFiltered.map((clientModel) {
+              return Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 5,
+                ),
+                child: InkWell(
+                  onTap: () {
+                    print(clientModel);
+                  },
+                  child: Card(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    color: colorCard,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: 15, right: 15, top: 10, bottom: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      clientModel.nickname,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 18),
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Nome: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        Text(
+                                          clientModel.name,
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w400),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Sobrenome: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        Text(
+                                          clientModel.lastName,
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w400),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'CPF: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        Text(
+                                          clientModel.cpf,
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w400),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Telefone: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        Text(
+                                          clientModel.phoneNumber,
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w400),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Data Nascimento: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        Text(
+                                          convertData(
+                                            clientModel.birthdate.toString(),
                                           ),
-                                          Text(
-                                            clientModel.name,
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                fontWeight:
-                                                FontWeight
-                                                    .w400),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text(
-                                            'Sobrenome: ',
-                                            style: TextStyle(
-                                                fontWeight:
-                                                FontWeight
-                                                    .w700),
-                                          ),
-                                          Text(
-                                            clientModel.lastName,
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                fontWeight:
-                                                FontWeight
-                                                    .w400),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text(
-                                            'CPF: ',
-                                            style: TextStyle(
-                                                fontWeight:
-                                                FontWeight
-                                                    .w700),
-                                          ),
-                                          Text(
-                                            clientModel.cpf,
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                fontWeight:
-                                                FontWeight
-                                                    .w400),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text(
-                                            'Telefone: ',
-                                            style: TextStyle(
-                                                fontWeight:
-                                                FontWeight
-                                                    .w700),
-                                          ),
-                                          Text(
-                                            clientModel.phoneNumber,
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                fontWeight:
-                                                FontWeight
-                                                    .w400),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text(
-                                            'Data Nascimento: ',
-                                            style: TextStyle(
-                                                fontWeight:
-                                                FontWeight
-                                                    .w700),
-                                          ),
-                                          Text(
-                                            convertData(
-                                              clientModel
-                                                  .birthdate
-                                                  .toString(),
-                                            ),
-                                            style: TextStyle(
-                                                color: Colors
-                                                    .blueAccent),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
+                                          style: TextStyle(
+                                              color: Colors.blueAccent),
+                                        )
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                );
-              }).toList(),
-            ));
+                ),
+              );
+            }).toList(),
+          ),
+          floatingActionButton: ButtonCreateClient(),
+        );
       },
     );
   }
