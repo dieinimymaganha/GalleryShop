5e12808ffdd45f7dc602e9edc8c20afd9e9731f4
Dieinimy <dieinimy@gmail.com>
2020-12-30 11:15:42 -0300

    Criado novo Dto da agenda

--- frontend/galleryshop/lib/models/schedule.dart
+++ frontend/galleryshop/lib/models/schedule.dart
@@ -1,3 +1,5 @@
+import 'client.dart';
+
 class ScheduleDto {
   int id;
   int employeeId;
@@ -73,5 +75,75 @@ class ScheduleAppointmentForm {
   @override
   String toString() {
     return 'ScheduleAppointmentForm{clientId: $clientId, avaliable: $avaliable}';
+  }
+}
+
+
+class ScheduleDtoAppointment {
+  int id;
+  int employeeId;
+  String nicknameEmployee;
+  String openingHours;
+  String day;
+  String typeEmployee;
+  String attendanceTime;
+  String startAttendance;
+  String endAttendance;
+  bool available;
+  int clientId;
+  String clientName;
+  ClientDto clientDto;
+
+  ScheduleDtoAppointment(
+      {this.id,
+        this.employeeId,
+        this.nicknameEmployee,
+        this.openingHours,
+        this.day,
+        this.typeEmployee,
+        this.attendanceTime,
+        this.startAttendance,
+        this.endAttendance,
+        this.available,
+        this.clientId,
+        this.clientName,
+        this.clientDto});
+
+  ScheduleDtoAppointment.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    employeeId = json['employeeId'];
+    nicknameEmployee = json['nicknameEmployee'];
+    openingHours = json['openingHours'];
+    day = json['day'];
+    typeEmployee = json['typeEmployee'];
+    attendanceTime = json['attendanceTime'];
+    startAttendance = json['startAttendance'];
+    endAttendance = json['endAttendance'];
+    available = json['available'];
+    clientId = json['clientId'];
+    clientName = json['clientName'];
+    clientDto = json['clientDto'] != null
+        ? new ClientDto.fromJson(json['clientDto'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['employeeId'] = this.employeeId;
+    data['nicknameEmployee'] = this.nicknameEmployee;
+    data['openingHours'] = this.openingHours;
+    data['day'] = this.day;
+    data['typeEmployee'] = this.typeEmployee;
+    data['attendanceTime'] = this.attendanceTime;
+    data['startAttendance'] = this.startAttendance;
+    data['endAttendance'] = this.endAttendance;
+    data['available'] = this.available;
+    data['clientId'] = this.clientId;
+    data['clientName'] = this.clientName;
+    if (this.clientDto != null) {
+      data['clientDto'] = this.clientDto.toJson();
+    }
+    return data;
   }
 }
