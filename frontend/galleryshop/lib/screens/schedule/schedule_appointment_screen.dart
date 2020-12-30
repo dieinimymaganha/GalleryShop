73fd56acb99c5790f8aa3e9a7d8f1552ee3ed3a9
Dieinimy <dieinimy@gmail.com>
2020-12-30 11:16:08 -0300

    Criado tela e card para tela de horários agendados.

---
+++ frontend/galleryshop/lib/screens/schedule/schedule_appointment_screen.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/screens/base/base_screen.dart';
+import 'package:galleryshop/screens/schedule/widgets/card_widget_schedule_appointment.dart';
+import 'package:galleryshop/stores/schedule_store.dart';
+import 'package:galleryshop/widgets/centered_message.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class ScheduleAppointmentScreen extends StatefulWidget {
+  @override
+  _ScheduleAppointmentScreenState createState() =>
+      _ScheduleAppointmentScreenState();
+}
+
+class _ScheduleAppointmentScreenState extends State<ScheduleAppointmentScreen> {
+  ScheduleStore scheduleStore = ScheduleStore(source: 'funcionario');
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Horários agendados'),
+            centerTitle: true,
+            backgroundColor: colorAppbar,
+            leading: IconButton(
+              icon: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => BaseScreen()));
+                },
+              ),
+            ),
+          ),
+          body: scheduleStore.errorList
+              ? Container(
+                  child: scheduleStore.listEmpty
+                      ? CenteredMessage(
+                          'Não á horários agendados',
+                          icon: Icons.description,
+                        )
+                      : Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24.0),
+                                child: Text(
+                                  'Falha ao carregar',
+                                  style: TextStyle(fontSize: 24),
+                                ),
+                              ),
+                              FlatButton(
+                                  child: Text('Clique para recarregar!'),
+                                  onPressed: scheduleStore.reloadList)
+                            ],
+                          ),
+                        ),
+                )
+              : scheduleStore.loadingPageScheduleTime
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 24.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TableCalendar(
+                              locale: 'pt_BR',
+                              events: scheduleStore.events,
+                              initialCalendarFormat: CalendarFormat.month,
+                              calendarStyle: CalendarStyle(
+                                outsideDaysVisible: false,
+                                weekendStyle: TextStyle()
+                                    .copyWith(color: Colors.blue[800]),
+                                holidayStyle: TextStyle()
+                                    .copyWith(color: Colors.blue[800]),
+                              ),
+                              availableCalendarFormats: const {
+                                CalendarFormat.month: 'Mês',
+                                CalendarFormat.week: 'Semana',
+                              },
+                              headerStyle: HeaderStyle(
+                                  centerHeaderTitle: true,
+                                  formatButtonDecoration: BoxDecoration(
+                                    color: Colors.blueGrey,
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  formatButtonTextStyle:
+                                      TextStyle(color: Colors.white),
+                                  formatButtonShowsNext: false),
+                              startingDayOfWeek: StartingDayOfWeek.monday,
+                              daysOfWeekStyle: DaysOfWeekStyle(
+                                weekendStyle: TextStyle()
+                                    .copyWith(color: Colors.blue[600]),
+                              ),
+                              onDaySelected: (date, events) {
+                                scheduleStore.setListMyScheduleAppointment();
+                                setState(() {
+                                  scheduleStore.selectedEvents = events;
+                                });
+                              },
+                              builders: CalendarBuilders(
+                                selectedDayBuilder: (context, date, events) =>
+                                    Container(
+                                        margin: EdgeInsets.all(4.0),
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                            color: colorAppbar,
+                                            shape: BoxShape.circle),
+                                        child: Text(
+                                          date.day.toString(),
+                                          style: TextStyle(color: Colors.white),
+                                        )),
+                                todayDayBuilder: (context, date, events) {
+                                  return Container(
+                                      margin: EdgeInsets.all(4),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          color: Colors.teal.shade300,
+                                          shape: BoxShape.circle),
+                                      child: Text(
+                                        date.day.toString(),
+                                        style: TextStyle(color: Colors.white),
+                                      ));
+                                },
+                                markersBuilder:
+                                    (context, date, events, holidays) {
+                                  final children = <Widget>[];
+
+                                  if (events.isNotEmpty) {
+                                    children.add(
+                                      Positioned(
+                                        right: 1,
+                                        bottom: 1,
+                                        child: _buildEventsMarker(date, events),
+                                      ),
+                                    );
+                                  }
+                                  return children;
+                                },
+                              ),
+                              calendarController:
+                                  scheduleStore.calendarController,
+                            ),
+                          ),
+                          Column(
+                              children: scheduleStore.selectedEvents
+                                  .map<Widget>((schedule) {
+                            return CardWidgetScheduleAppointment(
+                              scheduleDtoAppointment: schedule,
+                            );
+                          }).toList()),
+                        ],
+                      )),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scheduleStore.loadingInitPageAppointment();
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: scheduleStore.calendarController.isSelected(date)
+            ? Colors.green
+            : scheduleStore.calendarController.isToday(date)
+                ? Colors.brown[300]
+                : Colors.blue[400],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/galleryshop/lib/screens/schedule/widgets/card_widget_schedule_appointment.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:galleryshop/data/function_generic.dart';
+import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/models/schedule.dart';
+
+class CardWidgetScheduleAppointment extends StatelessWidget {
+  ScheduleDtoAppointment scheduleDtoAppointment;
+
+  CardWidgetScheduleAppointment({this.scheduleDtoAppointment});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Divider(
+          thickness: 1.0,
+        ),
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    scheduleDtoAppointment.startAttendance,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  lineGen(
+                    lines: [40.0, 30.0, 20.0],
+                  ),
+                  Text(
+                    scheduleDtoAppointment.endAttendance,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 12.0),
+            Expanded(
+              child: Container(
+                height: 100.0,
+                decoration: BoxDecoration(
+                    color: colorAppbar,
+                    borderRadius: (BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        bottomLeft: Radius.circular(8.0)))),
+                child: Container(
+                  margin: EdgeInsets.only(left: 4.0),
+                  color: colorCard,
+                  padding: EdgeInsets.only(
+                    left: 16.0,
+                    top: 8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 18.0,
+                        child: Row(
+                          children: <Widget>[
+                            Text('Cliente: '),
+                            Text(
+                              scheduleDtoAppointment.clientDto.name,
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                            Text(' | Apelido: '),
+                            Text(
+                              scheduleDtoAppointment.clientDto.nickname,
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 18.0,
+                        child: Row(
+                          children: <Widget>[
+                            Text('Data nasc: '),
+                            Text(
+                              convertData(
+                                  scheduleDtoAppointment.clientDto.birthdate),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.blueAccent),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.white70,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: FlatButton(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.event_busy,
+                                      color: Colors.red,
+                                    ),
+                                    Text(
+                                      'Cancelar horário',
+                                      style: TextStyle(color: Colors.red),
+                                    ),
+                                  ],
+                                ),
+                                onPressed: () {
+//                                  showDialog(
+//                                      context: context,
+//                                      builder: (context) =>
+//                                          DialogCancelAppointment(
+//                                            scheduleDto: scheduleDto,
+//                                          ));
+                                },
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class lineGen extends StatelessWidget {
+  final lines;
+
+  const lineGen({
+    Key key,
+    this.lines,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(
+            3,
+            (index) => Container(
+                  height: 2.0,
+                  width: lines[index],
+                  color: Color(0xffd0d2d8),
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                )));
+  }
+}
