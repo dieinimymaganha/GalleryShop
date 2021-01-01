64fba0fc5ef48ea76d12e5abfc73dd443aecd13a
Dieinimy <dieinimy@gmail.com>
2021-01-01 12:51:51 -0300

    Alterado para reaproveitar a tela de opções para consultar horarios disponiveis e horarios agendados

--- frontend/galleryshop/lib/screens/schedule/schedule_my_schedule_options.dart
+++ frontend/galleryshop/lib/screens/schedule/schedule_my_schedule_options.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/screens/schedule/schedule_appointment_screen.dart';
 import 'package:galleryshop/stores/schedule_store.dart';
 
 import 'schedule_my_schedule.dart';
 
 class ScheduleMyOptions extends StatefulWidget {
+  final bool appointmentConsult;
+
+  ScheduleMyOptions({this.appointmentConsult});
+
   @override
-  _ScheduleMyOptionsState createState() => _ScheduleMyOptionsState();
+  _ScheduleMyOptionsState createState() =>
+      _ScheduleMyOptionsState(appointmentConsult: appointmentConsult);
 }
 
 class _ScheduleMyOptionsState extends State<ScheduleMyOptions> {
+  _ScheduleMyOptionsState({bool appointmentConsult})
+      : scheduleStore = ScheduleStore(appointmentConsult: appointmentConsult);
+
   ScheduleStore scheduleStore = ScheduleStore();
 
   @override
   void initState() {
     super.initState();
-    scheduleStore.setOptionsMySchedule();
+    scheduleStore.loadingInitOptionsSchedule();
   }
 
   @override
@@ -25,61 +34,82 @@ class _ScheduleMyOptionsState extends State<ScheduleMyOptions> {
       builder: (_) {
         return Scaffold(
           appBar: AppBar(
-            title: Text('Agendas'),
+            title: Text(scheduleStore.appointmentConsult
+                ? 'Consultar horários agendados'
+                : 'Consultar horários disponíveis'),
             centerTitle: true,
             backgroundColor: colorAppbar,
           ),
-          body: scheduleStore.errorLoadingOptionsMySchedule
+          body: scheduleStore.loadingPageScheduleTime
               ? Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(top: 24.0),
-                        child: Text(
-                          'Falha ao carregar',
-                          style: TextStyle(fontSize: 24),
+                  child: CircularProgressIndicator(),
+                )
+              : scheduleStore.errorLoadingOptionsMySchedule
+                  ? Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 24.0),
+                            child: Text(
+                              'Falha ao carregar',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                          ),
+                          FlatButton(
+                              child: Text('Clique para recarregar!'),
+                              onPressed:
+                                  scheduleStore.realoadListOptionsMySchedule)
+                        ],
+                      ),
+                    )
+                  : SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 2, left: 8, right: 8),
+                        child: Column(
+                          children: scheduleStore.employeeDto.typeEmployees
+                              .map<Widget>((typeEmployee) {
+                            return Column(
+                              children: <Widget>[
+                                GestureDetector(
+                                  child: ListTile(
+                                    title: Text(typeEmployee.description),
+                                    trailing: Icon(Icons.keyboard_arrow_right),
+                                  ),
+                                  onTap: () {
+                                    scheduleStore.appointmentConsult
+                                        ? Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ScheduleAppointmentScreen(
+                                                      idEmployee: scheduleStore
+                                                          .employeeDto.id,
+                                                      idTypeEmployee:
+                                                          typeEmployee.id,
+                                                      appointmentConsult: true,
+                                                    )))
+                                        : Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ScheduleMySchedule(
+                                                      idEmployee: scheduleStore
+                                                          .employeeDto.id,
+                                                      idTypeEmployee:
+                                                          typeEmployee.id,
+                                                    )));
+                                  },
+                                ),
+                                Divider(
+                                  color: Colors.grey[400],
+                                  thickness: 0.5,
+                                )
+                              ],
+                            );
+                          }).toList(),
                         ),
                       ),
-                      FlatButton(
-                          child: Text('Clique para recarregar!'),
-                          onPressed: scheduleStore.realoadListOptionsMySchedule)
-                    ],
-                  ),
-                )
-              : SingleChildScrollView(
-                  child: Container(
-                    padding: EdgeInsets.only(top: 2, left: 8, right: 8),
-                    child: Column(
-                      children: scheduleStore.employeeDto.typeEmployees
-                          .map<Widget>((typeEmployee) {
-                        return Column(
-                          children: <Widget>[
-                            GestureDetector(
-                              child: ListTile(
-                                title: Text(typeEmployee.description),
-                                trailing: Icon(Icons.keyboard_arrow_right),
-                              ),
-                              onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => ScheduleMySchedule(
-                                          idEmployee:
-                                              scheduleStore.employeeDto.id,
-                                          idTypeEmployee: typeEmployee.id,
-                                        )));
-                              },
-                            ),
-                            Divider(
-                              color: Colors.grey[400],
-                              thickness: 0.5,
-                            )
-                          ],
-                        );
-                      }).toList(),
                     ),
-                  ),
-                ),
         );
       },
     );
