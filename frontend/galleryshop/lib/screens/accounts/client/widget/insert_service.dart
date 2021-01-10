83911b88ad089520332a41acc2c94c1f9bd8ff4e
Dieinimy <dieinimy@gmail.com>
2021-01-10 16:19:55 -0300

    Criado botão para inserir novo serviço.

---
+++ frontend/galleryshop/lib/screens/accounts/client/widget/insert_service.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:galleryshop/data/values.dart';
+
+class ButtonInsertService extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(
+        Icons.add,
+      ),
+      onPressed: () {
+//        Navigator.of(context)
+//            .push(MaterialPageRoute(builder: (context) => CreateNewUserClientScreen()));
+      },
+      backgroundColor: colorAppbar,
+    );
+  }
+}
