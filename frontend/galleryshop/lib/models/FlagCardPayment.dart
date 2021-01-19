84d0f886d0c246b0236478c1d2111c2dea4ba11f
Dieinimy <dieinimy@gmail.com>
2021-01-19 12:31:18 -0300

    Criado Dto para bandeira de cartão

---
+++ frontend/galleryshop/lib/models/FlagCardPayment.dart
@@ -0,0 +1,36 @@
+class FlagCardPaymentDto {
+  int id;
+  String description;
+  bool debit;
+  double taxDebit;
+  bool credit;
+  double taxCredit;
+
+  FlagCardPaymentDto(
+      {this.id,
+        this.description,
+        this.debit,
+        this.taxDebit,
+        this.credit,
+        this.taxCredit});
+
+  FlagCardPaymentDto.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    description = json['description'];
+    debit = json['debit'];
+    taxDebit = json['taxDebit'];
+    credit = json['credit'];
+    taxCredit = json['taxCredit'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['description'] = this.description;
+    data['debit'] = this.debit;
+    data['taxDebit'] = this.taxDebit;
+    data['credit'] = this.credit;
+    data['taxCredit'] = this.taxCredit;
+    return data;
+  }
+}
