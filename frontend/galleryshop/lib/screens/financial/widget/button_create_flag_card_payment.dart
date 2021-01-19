dece143f76e792ea9ef63547b11435ff980111a1
Dieinimy <dieinimy@gmail.com>
2021-01-19 17:43:35 -0300

    Incluido rota para cadastrar novo cartão

--- frontend/galleryshop/lib/screens/financial/widget/button_create_flag_card_payment.dart
+++ frontend/galleryshop/lib/screens/financial/widget/button_create_flag_card_payment.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/screens/financial/create_new_flag_card_payment_screen.dart';
 
 class ButtonCreateFlagCardPayment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      child: Icon(
-        Icons.credit_card
-      ),
-      onPressed: (){},
+      child: Icon(Icons.credit_card),
+      onPressed: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => CreateNewFlagCardPaymentScreen()));
+      },
       backgroundColor: colorAppbar,
     );
   }
