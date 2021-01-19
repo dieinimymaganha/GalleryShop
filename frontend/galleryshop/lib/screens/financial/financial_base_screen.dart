ccc8586c4c107870dc8eed6fec8ff3af22aaeb49
Dieinimy <dieinimy@gmail.com>
2021-01-19 12:31:32 -0300

    Criado base screen para financeiro

--- frontend/galleryshop/lib/screens/financial/financial_base_screen.dart
+++ frontend/galleryshop/lib/screens/financial/financial_base_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:galleryshop/data/values.dart';
+import 'package:galleryshop/screens/financial/flag_card_payment_list_screen.dart';
 import 'package:galleryshop/widgets/drawer/custom_drawer.dart';
 
 class FinancialBaseScreen extends StatelessWidget {
@@ -11,7 +12,29 @@ class FinancialBaseScreen extends StatelessWidget {
         backgroundColor: colorAppbar,
         centerTitle: true,
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(top: 2, left: 8, right: 8),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: ListTile(
+                  title: Text('Cartões'),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => FlagCardPaymentListScreen()));
+                },
+              ),
+              Divider(
+                color: Colors.grey[400],
+                thickness: 0.5,
+              ),
+            ],
+          ),
+        ),
+      ),
       drawer: CustomDrawer(),
     );
   }
