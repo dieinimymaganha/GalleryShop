c1a874a6b6f056578780cb9322b3e1d45f26a1ac
Dieinimy <dieinimy@gmail.com>
2021-01-19 17:44:11 -0300

    Criado logicas para criar novo cartão

--- frontend/galleryshop/lib/stores/financial_store.dart
+++ frontend/galleryshop/lib/stores/financial_store.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:galleryshop/http/webclients/webclient_financial.dart';
+import 'package:galleryshop/models/FlagCardPayment.dart';
 import 'package:mobx/mobx.dart';
 
 part 'financial_store.g.dart';
@@ -15,10 +17,84 @@ abstract class _FinancialStore with Store {
   FinancialWebClient financialWebClient = FinancialWebClient();
 
   @observable
+  TextEditingController controllerFieldCreditTax = TextEditingController();
+
+  @observable
+  TextEditingController controllerFieldDebitTax = TextEditingController();
+
+  @observable
+  TextEditingController controllerFieldDescription = TextEditingController();
+
+  @observable
+  GlobalKey<FormState> formState = GlobalKey<FormState>();
+
+  @observable
+  bool autoValidateForm = false;
+
+  @action
+  void setTaxCredit(String value) {
+    if (value.isEmpty) {
+      flagCardPaymentForm.taxCredit = null;
+    } else {
+      flagCardPaymentForm.taxCredit = double.parse(value);
+    }
+  }
+
+  @action
+  void setTaxDebit(String value) {
+    if (value.isEmpty) {
+      flagCardPaymentForm.taxDebit = null;
+    } else {
+      flagCardPaymentForm.taxDebit = double.parse(value);
+    }
+  }
+
+  @action
+  void setDescription(String value) => flagCardPaymentForm.description = value;
+
+  @observable
+  FlagCardPaymentForm flagCardPaymentForm;
+
+  @observable
   List<dynamic> dataFlagCardPayment = List();
 
   @action
   Future<void> initList() async {
     dataFlagCardPayment = await financialWebClient.findAll();
+  }
+
+  @observable
+  String description = '';
+
+  @observable
+  bool credit = false;
+
+  @observable
+  double taxCredit;
+
+  @observable
+  bool debit = false;
+
+  @observable
+  double taxDebit;
+
+  @action
+  Future<void> initCreateNew() async {
+    flagCardPaymentForm = FlagCardPaymentForm(
+        description: description,
+        credit: credit,
+        taxCredit: taxCredit,
+        debit: debit,
+        taxDebit: taxDebit);
+  }
+
+  @computed
+  bool get taxCreditValid {
+    if (flagCardPaymentForm.credit) {
+      if (flagCardPaymentForm.taxDebit != null) {
+        return true;
+      }
+    }
+    return false;
   }
 }
--- frontend/galleryshop/lib/stores/financial_store.g.dart
+++ frontend/galleryshop/lib/stores/financial_store.g.dart
@@ -9,6 +9,111 @@ part of 'financial_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$FinancialStore on _FinancialStore, Store {
+  Computed<bool> _$taxCreditValidComputed;
+
+  @override
+  bool get taxCreditValid =>
+      (_$taxCreditValidComputed ??= Computed<bool>(() => super.taxCreditValid,
+              name: '_FinancialStore.taxCreditValid'))
+          .value;
+
+  final _$controllerFieldCreditTaxAtom =
+      Atom(name: '_FinancialStore.controllerFieldCreditTax');
+
+  @override
+  TextEditingController get controllerFieldCreditTax {
+    _$controllerFieldCreditTaxAtom.reportRead();
+    return super.controllerFieldCreditTax;
+  }
+
+  @override
+  set controllerFieldCreditTax(TextEditingController value) {
+    _$controllerFieldCreditTaxAtom
+        .reportWrite(value, super.controllerFieldCreditTax, () {
+      super.controllerFieldCreditTax = value;
+    });
+  }
+
+  final _$controllerFieldDebitTaxAtom =
+      Atom(name: '_FinancialStore.controllerFieldDebitTax');
+
+  @override
+  TextEditingController get controllerFieldDebitTax {
+    _$controllerFieldDebitTaxAtom.reportRead();
+    return super.controllerFieldDebitTax;
+  }
+
+  @override
+  set controllerFieldDebitTax(TextEditingController value) {
+    _$controllerFieldDebitTaxAtom
+        .reportWrite(value, super.controllerFieldDebitTax, () {
+      super.controllerFieldDebitTax = value;
+    });
+  }
+
+  final _$controllerFieldDescriptionAtom =
+      Atom(name: '_FinancialStore.controllerFieldDescription');
+
+  @override
+  TextEditingController get controllerFieldDescription {
+    _$controllerFieldDescriptionAtom.reportRead();
+    return super.controllerFieldDescription;
+  }
+
+  @override
+  set controllerFieldDescription(TextEditingController value) {
+    _$controllerFieldDescriptionAtom
+        .reportWrite(value, super.controllerFieldDescription, () {
+      super.controllerFieldDescription = value;
+    });
+  }
+
+  final _$formStateAtom = Atom(name: '_FinancialStore.formState');
+
+  @override
+  GlobalKey<FormState> get formState {
+    _$formStateAtom.reportRead();
+    return super.formState;
+  }
+
+  @override
+  set formState(GlobalKey<FormState> value) {
+    _$formStateAtom.reportWrite(value, super.formState, () {
+      super.formState = value;
+    });
+  }
+
+  final _$autoValidateFormAtom = Atom(name: '_FinancialStore.autoValidateForm');
+
+  @override
+  bool get autoValidateForm {
+    _$autoValidateFormAtom.reportRead();
+    return super.autoValidateForm;
+  }
+
+  @override
+  set autoValidateForm(bool value) {
+    _$autoValidateFormAtom.reportWrite(value, super.autoValidateForm, () {
+      super.autoValidateForm = value;
+    });
+  }
+
+  final _$flagCardPaymentFormAtom =
+      Atom(name: '_FinancialStore.flagCardPaymentForm');
+
+  @override
+  FlagCardPaymentForm get flagCardPaymentForm {
+    _$flagCardPaymentFormAtom.reportRead();
+    return super.flagCardPaymentForm;
+  }
+
+  @override
+  set flagCardPaymentForm(FlagCardPaymentForm value) {
+    _$flagCardPaymentFormAtom.reportWrite(value, super.flagCardPaymentForm, () {
+      super.flagCardPaymentForm = value;
+    });
+  }
+
   final _$dataFlagCardPaymentAtom =
       Atom(name: '_FinancialStore.dataFlagCardPayment');
 
@@ -25,6 +130,81 @@ mixin _$FinancialStore on _FinancialStore, Store {
     });
   }
 
+  final _$descriptionAtom = Atom(name: '_FinancialStore.description');
+
+  @override
+  String get description {
+    _$descriptionAtom.reportRead();
+    return super.description;
+  }
+
+  @override
+  set description(String value) {
+    _$descriptionAtom.reportWrite(value, super.description, () {
+      super.description = value;
+    });
+  }
+
+  final _$creditAtom = Atom(name: '_FinancialStore.credit');
+
+  @override
+  bool get credit {
+    _$creditAtom.reportRead();
+    return super.credit;
+  }
+
+  @override
+  set credit(bool value) {
+    _$creditAtom.reportWrite(value, super.credit, () {
+      super.credit = value;
+    });
+  }
+
+  final _$taxCreditAtom = Atom(name: '_FinancialStore.taxCredit');
+
+  @override
+  double get taxCredit {
+    _$taxCreditAtom.reportRead();
+    return super.taxCredit;
+  }
+
+  @override
+  set taxCredit(double value) {
+    _$taxCreditAtom.reportWrite(value, super.taxCredit, () {
+      super.taxCredit = value;
+    });
+  }
+
+  final _$debitAtom = Atom(name: '_FinancialStore.debit');
+
+  @override
+  bool get debit {
+    _$debitAtom.reportRead();
+    return super.debit;
+  }
+
+  @override
+  set debit(bool value) {
+    _$debitAtom.reportWrite(value, super.debit, () {
+      super.debit = value;
+    });
+  }
+
+  final _$taxDebitAtom = Atom(name: '_FinancialStore.taxDebit');
+
+  @override
+  double get taxDebit {
+    _$taxDebitAtom.reportRead();
+    return super.taxDebit;
+  }
+
+  @override
+  set taxDebit(double value) {
+    _$taxDebitAtom.reportWrite(value, super.taxDebit, () {
+      super.taxDebit = value;
+    });
+  }
+
   final _$initListAsyncAction = AsyncAction('_FinancialStore.initList');
 
   @override
@@ -32,10 +212,66 @@ mixin _$FinancialStore on _FinancialStore, Store {
     return _$initListAsyncAction.run(() => super.initList());
   }
 
+  final _$initCreateNewAsyncAction =
+      AsyncAction('_FinancialStore.initCreateNew');
+
+  @override
+  Future<void> initCreateNew() {
+    return _$initCreateNewAsyncAction.run(() => super.initCreateNew());
+  }
+
+  final _$_FinancialStoreActionController =
+      ActionController(name: '_FinancialStore');
+
+  @override
+  void setTaxCredit(String value) {
+    final _$actionInfo = _$_FinancialStoreActionController.startAction(
+        name: '_FinancialStore.setTaxCredit');
+    try {
+      return super.setTaxCredit(value);
+    } finally {
+      _$_FinancialStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setTaxDebit(String value) {
+    final _$actionInfo = _$_FinancialStoreActionController.startAction(
+        name: '_FinancialStore.setTaxDebit');
+    try {
+      return super.setTaxDebit(value);
+    } finally {
+      _$_FinancialStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setDescription(String value) {
+    final _$actionInfo = _$_FinancialStoreActionController.startAction(
+        name: '_FinancialStore.setDescription');
+    try {
+      return super.setDescription(value);
+    } finally {
+      _$_FinancialStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-dataFlagCardPayment: ${dataFlagCardPayment}
+controllerFieldCreditTax: ${controllerFieldCreditTax},
+controllerFieldDebitTax: ${controllerFieldDebitTax},
+controllerFieldDescription: ${controllerFieldDescription},
+formState: ${formState},
+autoValidateForm: ${autoValidateForm},
+flagCardPaymentForm: ${flagCardPaymentForm},
+dataFlagCardPayment: ${dataFlagCardPayment},
+description: ${description},
+credit: ${credit},
+taxCredit: ${taxCredit},
+debit: ${debit},
+taxDebit: ${taxDebit},
+taxCreditValid: ${taxCreditValid}
     ''';
   }
 }
