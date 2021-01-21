c1734b77f6c11a2903d17125510303d37e76b46a
Dieinimy <dieinimy@gmail.com>
2021-01-21 13:22:51 -0300

    Criado consulta para buscar os pagamentos por cliente e data de pagamento.

--- backend/GalleryShop/src/main/java/GalleryShop/repository/PaymentRepository.java
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/PaymentRepository.java
@@ -1,10 +1,17 @@
 package GalleryShop.repository;
 
 import GalleryShop.model.Payment;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByAccountClientId(Long id);
+
+    @Query(value = "select * from payment where account_client_id = ? " +
+            "and date_payment = ? order by date_payment", nativeQuery = true)
+    List<Payment> findByAccountClientIDAndDatePayment(Long id, Date date);
 }
