866590c555a316eb4a5f45de668a869cbcd76cb9
Dieinimy <dieinimy@gmail.com>
2021-01-22 17:15:12 -0300

    Alterado query para buscar os serviços.

--- backend/GalleryShop/src/main/java/GalleryShop/repository/ServiceRecordRepository.java
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/ServiceRecordRepository.java
@@ -10,11 +10,19 @@ import java.util.List;
 public interface ServiceRecordRepository extends JpaRepository<ServiceRecord, Long> {
 
 
-    @Query(value = "select * from service_record s " +
-            "inner join account_client ac on s.account_id = ac.id " +
-            "inner join billed_service b ON b.id = s.billed_service_id " +
-            "inner join payment p ON p.account_client_id = ac.id where ac.client_id = ? " +
-            "and s.date_service > p.date_payment", nativeQuery = true)
+//    @Query(value = "select * from service_record s " +
+//            "inner join account_client ac on s.account_id = ac.id " +
+//            "inner join billed_service b ON b.id = s.billed_service_id " +
+//            "inner join payment p ON p.account_client_id = ac.id where ac.client_id = ? " +
+//            "and s.date_service > p.date_payment", nativeQuery = true)
+//    List<ServiceRecord> findByClientId(Long id);
+
+    @Query(value = "select * from service_record s \n" +
+            "inner join account_client ac on s.account_id = ac.id \n" +
+            "inner join billed_service b ON b.id = s.billed_service_id \n" +
+            "where ac.client_id = ?", nativeQuery = true)
     List<ServiceRecord> findByClientId(Long id);
+
+
 
 }
