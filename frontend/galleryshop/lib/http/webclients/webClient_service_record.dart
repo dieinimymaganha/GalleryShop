4f7be5c020480479ac679bb9648f09d131bae0d0
Dieinimy <dieinimy@gmail.com>
2021-01-22 11:04:21 -0300

    Criado chamada o endPoint para excluir um serviço

--- frontend/galleryshop/lib/http/webclients/webClient_service_record.dart
+++ frontend/galleryshop/lib/http/webclients/webClient_service_record.dart
@@ -21,6 +21,39 @@ class ServiceRecordWebClient {
     return response.statusCode;
   }
 
+  Future<List<ServiceRecordDto>> findByClientId(int id) async {
+    String token = await getToken();
+    final response = await webClient.get(
+      urlServiceRecord + "/clientId=" + id.toString(),
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': "Bearer $token"
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> decodeJson = jsonDecode(response.body);
+      final List<dynamic> data = decodeJson
+          .map((dynamic json) => ServiceRecordDto.fromJson(json))
+          .toList();
+      return data;
+    }
+    throw HttpException(_getMessage(response.statusCode));
+  }
+
+  Future<int> exclude(int id) async {
+    String token = await getToken();
+    String urlExclude = urlServiceRecord + '/' + id.toString();
+    final Response response = await webClient.delete(
+      urlExclude,
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': "Bearer $token",
+      },
+    );
+    return response.statusCode;
+  }
+
   String _getMessage(int statuscode) {
     if (_statusCodeResponses.containsKey(statuscode)) {
       return _statusCodeResponses[statuscode];
