079181d656b83a0dcc4b5a9075f07aacdfe5e220
Dieinimy <dieinimy@gmail.com>
2021-01-22 17:14:07 -0300

    Criado form para fechar conta.

--- frontend/galleryshop/lib/models/AccountClient.dart
+++ frontend/galleryshop/lib/models/AccountClient.dart
@@ -212,3 +212,46 @@ class ServiceRecordForm {
 }
 
 
+class CloseAccountClientForm {
+  int idAccountClient;
+  double value;
+  int idFlagCardPayment;
+  bool card;
+  bool creditCard;
+  bool debitCard;
+  bool useBalance;
+
+  CloseAccountClientForm(
+      {this.idAccountClient,
+        this.value,
+        this.idFlagCardPayment,
+        this.card,
+        this.creditCard,
+        this.debitCard,
+        this.useBalance});
+
+  CloseAccountClientForm.fromJson(Map<String, dynamic> json) {
+    idAccountClient = json['idAccountClient'];
+    value = json['value'];
+    idFlagCardPayment = json['idFlagCardPayment'];
+    card = json['card'];
+    creditCard = json['creditCard'];
+    debitCard = json['debitCard'];
+    useBalance = json['useBalance'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['idAccountClient'] = this.idAccountClient;
+    data['value'] = this.value;
+    data['idFlagCardPayment'] = this.idFlagCardPayment;
+    data['card'] = this.card;
+    data['creditCard'] = this.creditCard;
+    data['debitCard'] = this.debitCard;
+    data['useBalance'] = this.useBalance;
+    return data;
+  }
+}
+
+
+
