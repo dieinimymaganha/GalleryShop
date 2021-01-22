7ac60ae23583d86f1c6630bc669c9f9156aa0398
Dieinimy <dieinimy@gmail.com>
2021-01-22 11:05:46 -0300

    Alterado local da classe do menu do detalhe da conta do cliente

---
+++ frontend/galleryshop/lib/screens/accounts/client/widget/option_menu_detail_account_client.dart
@@ -0,0 +1,14 @@
+class OptionsMenuDetailClient {
+  static const String excludeServices = 'Excluir serviços';
+  static const String insertServices = 'Inserir serviços';
+  static const String consultPayment = 'Consultar pagamentos';
+  static const String updatePage = 'Atualizar';
+
+
+  static const List<String> choices = <String>[
+    updatePage,
+    insertServices,
+    excludeServices,
+    consultPayment,
+  ];
+}
