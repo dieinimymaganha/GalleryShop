65f54fc459ea6d61c76b3a73eda3fd0176ba7458
Dieinimy <dieinimy@gmail.com>
2021-01-26 14:11:01 -0300

    Criado toString dto productSold

--- frontend/galleryshop/lib/models/product_sold.dart
+++ frontend/galleryshop/lib/models/product_sold.dart
@@ -1,9 +1,9 @@
 class ProductSoldDto {
   int id;
   String description;
-  int value;
+  double value;
   int quantity;
-  int valueTotal;
+  double valueTotal;
 
   ProductSoldDto(
       {this.id, this.description, this.value, this.quantity, this.valueTotal});
@@ -24,5 +24,10 @@ class ProductSoldDto {
     data['quantity'] = this.quantity;
     data['valueTotal'] = this.valueTotal;
     return data;
+  }
+
+  @override
+  String toString() {
+    return 'ProductSoldDto{id: $id, description: $description, value: $value, quantity: $quantity, valueTotal: $valueTotal}';
   }
 }
