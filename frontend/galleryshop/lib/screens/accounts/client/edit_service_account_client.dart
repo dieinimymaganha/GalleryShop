15743aed3fc674cfaff37d36a92f6decc1ea76c5
Dieinimy <dieinimy@gmail.com>
2021-01-26 16:09:08 -0300

    Removido icone de exclusão e alterado pra LongPressed

--- frontend/galleryshop/lib/screens/accounts/client/edit_service_account_client.dart
+++ frontend/galleryshop/lib/screens/accounts/client/edit_service_account_client.dart
@@ -56,185 +56,194 @@ class _EditServiceAccountClientScreenState
               ),
             ),
           ),
-          body: billedServiceStore.errorList ? Container(
-            child: billedServiceStore.listEmpty ? CenteredMessage(
-              'Não á serviços registrados',
-              icon: Icons.description
-            ) : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24.0),
-                    child: Text('Falha do carregar', style: TextStyle(fontSize: 24.0),),
-                  ),
-                  FlatButton(
-                    child: Text('Clique para recarregar'),
-                    onPressed: billedServiceStore.reloadPageExcludeService,
-                  )
-                ],
-              ),
-            ),
-          ) : Container(
-            padding: EdgeInsets.all(10.0),
-            child: ListView(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TableCalendar(
-                    locale: 'pt_BR',
-                    events: billedServiceStore.events,
-                    initialCalendarFormat: CalendarFormat.month,
-                    calendarStyle: CalendarStyle(
-                      outsideDaysVisible: false,
-                      weekendStyle:
-                          TextStyle().copyWith(color: Colors.blue[800]),
-                      holidayStyle:
-                          TextStyle().copyWith(color: Colors.blue[800]),
-                    ),
-                    availableCalendarFormats: const {
-                      CalendarFormat.month: 'Mês',
-                      CalendarFormat.week: 'Semana',
-                    },
-                    headerStyle: HeaderStyle(
-                        centerHeaderTitle: true,
-                        formatButtonDecoration: BoxDecoration(
-                          color: Colors.blueGrey,
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        formatButtonTextStyle: TextStyle(color: Colors.white),
-                        formatButtonShowsNext: false),
-                    startingDayOfWeek: StartingDayOfWeek.monday,
-                    daysOfWeekStyle: DaysOfWeekStyle(
-                      weekendStyle:
-                          TextStyle().copyWith(color: Colors.blue[600]),
-                    ),
-                    onDaySelected: (date, events) {
-                      billedServiceStore.setListCalendar();
-                      setState(() {
-                        billedServiceStore.selectedEvents = events;
-                      });
-                    },
-                    builders: CalendarBuilders(
-                      selectedDayBuilder: (context, date, events) => Container(
-                          margin: EdgeInsets.all(4.0),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: colorAppbar, shape: BoxShape.circle),
-                          child: Text(
-                            date.day.toString(),
-                            style: TextStyle(color: Colors.white),
-                          )),
-                      todayDayBuilder: (context, date, events) {
-                        return Container(
-                            margin: EdgeInsets.all(4),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: Colors.teal.shade300,
-                                shape: BoxShape.circle),
-                            child: Text(
-                              date.day.toString(),
-                              style: TextStyle(color: Colors.white),
-                            ));
-                      },
-                      markersBuilder: (context, date, events, holidays) {
-                        final children = <Widget>[];
-
-                        if (events.isNotEmpty) {
-                          children.add(
-                            Positioned(
-                              right: 1,
-                              bottom: 1,
-                              child: _buildEventsMarker(date, events),
-                            ),
-                          );
-                        }
-                        return children;
-                      },
-                    ),
-                    calendarController: billedServiceStore.calendarController,
-                  ),
-                ),
-                Column(
-                  children: billedServiceStore.selectedEvents
-                      .map<Widget>((serviceRecordDto) {
-                    return Row(
-                      children: <Widget>[
-                        Expanded(
-                            child: Card(
-                              color: colorCard,
-                              child: ListTile(
-                                title: Text(serviceRecordDto.billedServiceDto.description),
-                                subtitle: Column(
-                                  children: <Widget>[
-                                    Row(
-                                      children: <Widget>[
-                                        Text(
-                                          'Funcionário : ',
-                                          style: TextStyle(fontWeight: FontWeight.w500),
-                                        ),
-                                        Text(
-                                            '${serviceRecordDto.employeeDtoBasic.name} | ${serviceRecordDto.billedServiceDto.typeEmployee}'),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Text(
-                                          'Valor : ',
-                                          style: TextStyle(fontWeight: FontWeight.w500),
-                                        ),
-                                        Text(convertMonetary(serviceRecordDto.billedServiceDto.value))
-                                      ],
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Text(
-                                          'Desconto : ',
-                                          style: TextStyle(fontWeight: FontWeight.w500),
-                                        ),
-                                        Text(
-                                            convertMonetary(serviceRecordDto.billedServiceDto.discount))
-                                      ],
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Text(
-                                          'Valor cobrado : ',
-                                          style: TextStyle(fontWeight: FontWeight.w500),
-                                        ),
-                                        Text(convertMonetary(
-                                            serviceRecordDto.billedServiceDto.valueFinal))
-                                      ],
-                                    ),
-                                  ],
+          body: billedServiceStore.errorList
+              ? Container(
+                  child: billedServiceStore.listEmpty
+                      ? CenteredMessage('Não á serviços registrados',
+                          icon: Icons.description)
+                      : Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24.0),
+                                child: Text(
+                                  'Falha do carregar',
+                                  style: TextStyle(fontSize: 24.0),
                                 ),
-                                trailing: IconButton(
-                                  icon: Icon(
-                                    Icons.delete,
-                                    color: Colors.redAccent,
+                              ),
+                              FlatButton(
+                                child: Text('Clique para recarregar'),
+                                onPressed:
+                                    billedServiceStore.reloadPageExcludeService,
+                              )
+                            ],
+                          ),
+                        ),
+                )
+              : Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: ListView(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TableCalendar(
+                          locale: 'pt_BR',
+                          events: billedServiceStore.events,
+                          initialCalendarFormat: CalendarFormat.month,
+                          calendarStyle: CalendarStyle(
+                            outsideDaysVisible: false,
+                            weekendStyle:
+                                TextStyle().copyWith(color: Colors.blue[800]),
+                            holidayStyle:
+                                TextStyle().copyWith(color: Colors.blue[800]),
+                          ),
+                          availableCalendarFormats: const {
+                            CalendarFormat.month: 'Mês',
+                            CalendarFormat.week: 'Semana',
+                          },
+                          headerStyle: HeaderStyle(
+                              centerHeaderTitle: true,
+                              formatButtonDecoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              formatButtonTextStyle:
+                                  TextStyle(color: Colors.white),
+                              formatButtonShowsNext: false),
+                          startingDayOfWeek: StartingDayOfWeek.monday,
+                          daysOfWeekStyle: DaysOfWeekStyle(
+                            weekendStyle:
+                                TextStyle().copyWith(color: Colors.blue[600]),
+                          ),
+                          onDaySelected: (date, events) {
+                            billedServiceStore.setListCalendar();
+                            setState(() {
+                              billedServiceStore.selectedEvents = events;
+                            });
+                          },
+                          builders: CalendarBuilders(
+                            selectedDayBuilder: (context, date, events) =>
+                                Container(
+                                    margin: EdgeInsets.all(4.0),
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: colorAppbar,
+                                        shape: BoxShape.circle),
+                                    child: Text(
+                                      date.day.toString(),
+                                      style: TextStyle(color: Colors.white),
+                                    )),
+                            todayDayBuilder: (context, date, events) {
+                              return Container(
+                                  margin: EdgeInsets.all(4),
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      color: Colors.teal.shade300,
+                                      shape: BoxShape.circle),
+                                  child: Text(
+                                    date.day.toString(),
+                                    style: TextStyle(color: Colors.white),
+                                  ));
+                            },
+                            markersBuilder: (context, date, events, holidays) {
+                              final children = <Widget>[];
+
+                              if (events.isNotEmpty) {
+                                children.add(
+                                  Positioned(
+                                    right: 1,
+                                    bottom: 1,
+                                    child: _buildEventsMarker(date, events),
                                   ),
-                                  onPressed: () {
+                                );
+                              }
+                              return children;
+                            },
+                          ),
+                          calendarController:
+                              billedServiceStore.calendarController,
+                        ),
+                      ),
+                      Column(
+                        children: billedServiceStore.selectedEvents
+                            .map<Widget>((serviceRecordDto) {
+                          return Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Card(
+                                color: colorCard,
+                                child: ListTile(
+                                  title: Text(serviceRecordDto
+                                      .billedServiceDto.description),
+                                  subtitle: Column(
+                                    children: <Widget>[
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Funcionário : ',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                          Text(
+                                              '${serviceRecordDto.employeeDtoBasic.name} | ${serviceRecordDto.billedServiceDto.typeEmployee}'),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Valor : ',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                          Text(convertMonetary(serviceRecordDto
+                                              .billedServiceDto.value))
+                                        ],
+                                      ),
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Desconto : ',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                          Text(convertMonetary(serviceRecordDto
+                                              .billedServiceDto.discount))
+                                        ],
+                                      ),
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Valor cobrado : ',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                          Text(convertMonetary(serviceRecordDto
+                                              .billedServiceDto.valueFinal))
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  onLongPress: () {
                                     showDialog(
                                         context: context,
                                         builder: (context) =>
                                             DialogExcludeServiceRecord(
-                                              idService:
-                                              serviceRecordDto.id,
-                                              idClient: billedServiceStore
-                                                  .idClient,
+                                              idService: serviceRecordDto.id,
+                                              idClient:
+                                                  billedServiceStore.idClient,
                                             ));
                                   },
                                 ),
-                              ),
-                            )),
-                      ],
-                    );
-                  }).toList(),
-                )
-              ],
-            ),
-          ),
+                              )),
+                            ],
+                          );
+                        }).toList(),
+                      )
+                    ],
+                  ),
+                ),
         );
       },
     );
