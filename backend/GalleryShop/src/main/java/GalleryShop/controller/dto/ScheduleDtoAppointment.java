63ee70c4df09081d5d5b4c4a95f4a7904a7510ac
Dieinimy <dieinimy@gmail.com>
2021-01-27 10:17:37 -0300

    Alterado o retorno do Dto de horarios agendados.

--- backend/GalleryShop/src/main/java/GalleryShop/controller/dto/ScheduleDtoAppointment.java
+++ backend/GalleryShop/src/main/java/GalleryShop/controller/dto/ScheduleDtoAppointment.java
@@ -2,6 +2,7 @@ package GalleryShop.controller.dto;
 
 import GalleryShop.model.Schedule;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,13 +10,25 @@ public class ScheduleDtoAppointment extends ScheduleDto {
 
     private Long clientId;
     private String clientName;
+    private Boolean concluded;
+    private LocalTime completionTime;
     private ClientDto clientDto;
 
     public ScheduleDtoAppointment(final Schedule schedule) {
         super(schedule);
         this.clientId = schedule.getClient().getId();
         this.clientName = schedule.getClient().getName();
+        this.concluded = schedule.getConcluded();
+        this.completionTime = schedule.getCompletionTime();
         this.clientDto = new ClientDto(schedule.getClient());
+    }
+
+    public LocalTime getCompletionTime() {
+        return completionTime;
+    }
+
+    public Boolean getConcluded() {
+        return concluded;
     }
 
     public ClientDto getClientDto() {
