763c433414667e6bc656591a4d0b5e3ceb3143f3
Dieinimy <dieinimy@gmail.com>
2021-01-28 14:31:40 -0300

    Alterado e criado busca correta dos produtos a serem alterados na venda

--- backend/GalleryShop/src/main/java/GalleryShop/repository/ProductSoldRepository.java
+++ backend/GalleryShop/src/main/java/GalleryShop/repository/ProductSoldRepository.java
@@ -9,9 +9,18 @@ import java.util.Optional;
 
 public interface ProductSoldRepository extends JpaRepository<ProductSold, Long> {
 
-    @Query(value = "select * from product_sold inner join sale "  +
-            "on product_sold.id = sale.product_sold_id " +
-            "where sale.date_sale = CURRENT_DATE and product_sold.description = ?", nativeQuery = true)
-    Optional<ProductSold> findByProductSoldDescription(String description);
+    @Query(value = "select * from product_sold p " +
+            "inner join sale s ON s.product_sold_id = p.id " +
+            "inner join account_client c ON c.id = s.account_id " +
+            "where s.date_sale = CURRENT_DATE and c.client_id = ? and p.description = ?", nativeQuery = true)
+    Optional<ProductSold> findByProductSoldIdClientDescription(Long id, String description);
+
+
+    @Query(value = "select * from product_sold p " +
+            "inner join sale s ON s.product_sold_id = p.id " +
+            "inner join account_employee c ON c.id = s.account_employee_id " +
+            "where s.date_sale = CURRENT_DATE and c.employee_id = ? and p.description = ?", nativeQuery = true)
+    Optional<ProductSold> findByProductSoldIdEmployeeDescription(Long id, String description);
+
 
 }
